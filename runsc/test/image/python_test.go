0d9bba0fc75f5bf1bc08b3b09fe095d86f400875
Justine Olshan <justineolshan@google.com>
2018-07-06 09:37:32 -0700

    Tests pause and resume functionality on a Python container.
    PiperOrigin-RevId: 203488336

---
+++ runsc/test/image/python_test.go
@@ -0,0 +1,51 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package image
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestPythonHello(t *testing.T) {
+	d := makeDocker("python-hello-test")
+	if out, err := d.run("-p", "8080", "google/python-hello"); err != nil {
+		t.Fatalf("docker run failed: %v\nout: %s", err, out)
+	}
+	defer d.cleanUp()
+
+	// Find where port 8080 is mapped to.
+	port, err := d.findPort(8080)
+	if err != nil {
+		t.Fatalf("docker.findPort(8080) failed: %v", err)
+	}
+
+	// Wait until it's up and running.
+	if err := d.waitForHTTP(port, 5*time.Second); err != nil {
+		t.Fatalf("docker.WaitForHTTP() timeout: %v", err)
+	}
+
+	// Ensure that content is being served.
+	url := fmt.Sprintf("http://localhost:%d", port)
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Errorf("Error reaching http server: %v", err)
+	}
+	if want := http.StatusOK; resp.StatusCode != want {
+		t.Errorf("Wrong response code, got: %d, want: %d", resp.StatusCode, want)
+	}
+}
