b935311e2371abdbceba89294d0001905f2658d5
Jamie Liu <jamieliu@google.com>
2018-08-31 14:16:36 -0700

    Do not use fs.FileOwnerFromContext in fs/proc.file.UnstableAttr().
    From //pkg/sentry/context/context.go:
    
    // - It is *not safe* to retain a Context passed to a function beyond the scope
    // of that function call.
    
    Passing a stored kernel.Task as a context.Context to
    fs.FileOwnerFromContext violates this requirement.
    
    PiperOrigin-RevId: 211143021
    Change-Id: I4c5b02bd941407be4c9cfdbcbdfe5a26acaec037

--- pkg/sentry/fs/proc/file.go
+++ pkg/sentry/fs/proc/file.go
@@ -51,7 +51,8 @@ func (f *file) UnstableAttr(ctx context.Context, inode *fs.Inode) (fs.UnstableAt
 		return fs.UnstableAttr{}, err
 	}
 	if f.t != nil {
-		uattr.Owner = fs.FileOwnerFromContext(f.t)
+		creds := f.t.Credentials()
+		uattr.Owner = fs.FileOwner{creds.EffectiveKUID, creds.EffectiveKGID}
 	}
 	return uattr, nil
 }
