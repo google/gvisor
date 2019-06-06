57772db2e7351511de422baeecf807785709ee5d
Michael Pratt <mpratt@google.com>
2019-06-05 18:39:30 -0700

    Shutdown host sockets on internal shutdown
    This is required to make the shutdown visible to peers outside the
    sandbox.
    
    The readClosed / writeClosed fields were dropped, as they were
    preventing a shutdown socket from reading the remainder of queued bytes.
    The host syscalls will return the appropriate errors for shutdown.
    
    The control message tests have been split out of socket_unix.cc to make
    the (few) remaining tests accessible to testing inherited host UDS,
    which don't support sending control messages.
    
    Updates #273
    
    PiperOrigin-RevId: 251763060

--- test/syscalls/linux/socket_abstract.cc
+++ test/syscalls/linux/socket_abstract.cc
@@ -17,6 +17,7 @@
 #include "test/syscalls/linux/socket_generic.h"
 #include "test/syscalls/linux/socket_test_util.h"
 #include "test/syscalls/linux/socket_unix.h"
+#include "test/syscalls/linux/socket_unix_cmsg.h"
 #include "test/syscalls/linux/unix_domain_socket_test_util.h"
 #include "test/util/test_util.h"
 
@@ -36,6 +37,10 @@ INSTANTIATE_TEST_SUITE_P(
 
 INSTANTIATE_TEST_SUITE_P(
     AbstractUnixSockets, UnixSocketPairTest,
+    ::testing::ValuesIn(IncludeReversals(GetSocketPairs())));
+
+INSTANTIATE_TEST_SUITE_P(
+    AbstractUnixSockets, UnixSocketPairCmsgTest,
     ::testing::ValuesIn(IncludeReversals(GetSocketPairs())));
 
 }  // namespace testing
