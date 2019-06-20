292f70cbf7b4d2da9f2fc5d1049ba49e6846e805
Michael Pratt <mpratt@google.com>
2019-06-20 13:33:29 -0700

    Add package docs to seqfile and ramfs
    These are the only packages missing docs:
    https://godoc.org/gvisor.dev/gvisor
    
    PiperOrigin-RevId: 254261022

--- pkg/sentry/fs/proc/seqfile/seqfile.go
+++ pkg/sentry/fs/proc/seqfile/seqfile.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package seqfile provides dynamic ordered files.
 package seqfile
 
 import (
