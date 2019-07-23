5ddf9adb2b12a2cbccdcf609c5cc140fa0dbd81d
Kevin Krakauer <krakauer@google.com>
2019-07-22 17:05:02 -0700

    Fix up and add some iptables ABI.
    PiperOrigin-RevId: 259437060

---
+++ pkg/abi/linux/netfilter_test.go
@@ -0,0 +1,45 @@
+// Copyright 2019 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package linux
+
+import (
+	"testing"
+
+	"gvisor.dev/gvisor/pkg/binary"
+)
+
+func TestSizes(t *testing.T) {
+	testCases := []struct {
+		typ     interface{}
+		defined uintptr
+	}{
+		{IPTEntry{}, SizeOfIPTEntry},
+		{IPTGetEntries{}, SizeOfIPTGetEntries},
+		{IPTGetinfo{}, SizeOfIPTGetinfo},
+		{IPTIP{}, SizeOfIPTIP},
+		{IPTReplace{}, SizeOfIPTReplace},
+		{XTCounters{}, SizeOfXTCounters},
+		{XTEntryMatch{}, SizeOfXTEntryMatch},
+		{XTEntryTarget{}, SizeOfXTEntryTarget},
+		{XTErrorTarget{}, SizeOfXTErrorTarget},
+		{XTStandardTarget{}, SizeOfXTStandardTarget},
+	}
+
+	for _, tc := range testCases {
+		if calculated := binary.Size(tc.typ); calculated != tc.defined {
+			t.Errorf("%T has a defined size of %d and calculated size of %d", tc.typ, tc.defined, calculated)
+		}
+	}
+}
