b6a5b950d28e0b474fdad160b88bc15314cf9259
Kevin Krakauer <krakauer@google.com>
2019-08-02 14:04:28 -0700

    Job control: controlling TTYs and foreground process groups.
    (Don't worry, this is mostly tests.)
    
    Implemented the following ioctls:
    - TIOCSCTTY - set controlling TTY
    - TIOCNOTTY - remove controlling tty, maybe signal some other processes
    - TIOCGPGRP - get foreground process group. Also enables tcgetpgrp().
    - TIOCSPGRP - set foreground process group. Also enabled tcsetpgrp().
    
    Next steps are to actually turn terminal-generated control characters (e.g. C^c)
    into signals to the proper process groups, and to send SIGTTOU and SIGTTIN when
    appropriate.
    
    PiperOrigin-RevId: 261387276

---
+++ test/util/pty_util.h
@@ -0,0 +1,30 @@
+// Copyright 2019 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GVISOR_TEST_UTIL_PTY_UTIL_H_
+#define GVISOR_TEST_UTIL_PTY_UTIL_H_
+
+#include "test/util/file_descriptor.h"
+#include "test/util/posix_error.h"
+
+namespace gvisor {
+namespace testing {
+
+// Opens the slave end of the passed master as R/W and nonblocking.
+PosixErrorOr<FileDescriptor> OpenSlave(const FileDescriptor& master);
+
+}  // namespace testing
+}  // namespace gvisor
+
+#endif  // GVISOR_TEST_UTIL_PTY_UTIL_H_
