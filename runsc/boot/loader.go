4fb520d44b24b4d924fa503f473e2cf02887d725
Fabricio Voznika <fvoznika@google.com>
2019-08-02 16:33:51 -0700

    Remove stale TODO
    This was done in commit 445814d2dd8316488910f347c948a7f8994ec5a1
    
    PiperOrigin-RevId: 261414748

--- runsc/boot/loader.go
+++ runsc/boot/loader.go
@@ -630,7 +630,6 @@ func (l *Loader) startContainer(spec *specs.Spec, conf *Config, cid string, file
 	// sentry currently supports only 1 mount namespace, which is tied to a
 	// single user namespace. Thus we must run in the same user namespace
 	// to access mounts.
-	// TODO(b/63601033): Create a new mount namespace for the container.
 	creds := auth.NewUserCredentials(
 		auth.KUID(spec.Process.User.UID),
 		auth.KGID(spec.Process.User.GID),
