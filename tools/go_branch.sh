d1f74f7aea9c8d1ce304eb547050cccd94442a7d
Adin Scannell <ascannell@google.com>
2019-11-25 13:51:13 -0800

    Fix typo in go_branch.sh script.
    With the ticks, the command `master` is actually be run and the output included
    (which is nothing). This is confusing, as we actually mean to say "master" in
    the description of the Go branch.
    
    PiperOrigin-RevId: 282426081

--- tools/go_branch.sh
+++ tools/go_branch.sh
@@ -78,7 +78,7 @@ cat > README.md <<EOF
 # gVisor
 
 This branch is a synthetic branch, containing only Go sources, that is
-compatible with standard Go tools. See the `master` branch for authoritative
+compatible with standard Go tools. See the master branch for authoritative
 sources and tests.
 EOF
 
