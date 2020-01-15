f874723e64bd8a2e747bb336e0b6b8f0da1f044a
Bhasker Hariharan <bhaskerh@google.com>
2020-01-15 11:17:25 -0800

    Bump SO_SNDBUF for fdbased endpoint used by runsc.
    Updates #231
    
    PiperOrigin-RevId: 289897881

--- benchmarks/tcp/tcp_proxy.go
+++ benchmarks/tcp/tcp_proxy.go
@@ -84,8 +84,8 @@ func (netImpl) printStats() {
 }
 
 const (
-	nicID      = 1       // Fixed.
-	rcvBufSize = 4 << 20 // 1MB.
+	nicID   = 1       // Fixed.
+	bufSize = 4 << 20 // 4MB.
 )
 
 type netstackImpl struct {
@@ -125,13 +125,13 @@ func setupNetwork(ifaceName string, numChannels int) (fds []int, err error) {
 			}
 
 			// RAW Sockets by default have a very small SO_RCVBUF of 256KB,
-			// up it to at least 1MB to reduce packet drops.
-			if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, rcvBufSize); err != nil {
-				return nil, fmt.Errorf("setsockopt(..., SO_RCVBUF, %v,..) = %v", rcvBufSize, err)
+			// up it to at least 4MB to reduce packet drops.
+			if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, bufSize); err != nil {
+				return nil, fmt.Errorf("setsockopt(..., SO_RCVBUF, %v,..) = %v", bufSize, err)
 			}
 
-			if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_SNDBUF, rcvBufSize); err != nil {
-				return nil, fmt.Errorf("setsockopt(..., SO_RCVBUF, %v,..) = %v", rcvBufSize, err)
+			if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_SNDBUF, bufSize); err != nil {
+				return nil, fmt.Errorf("setsockopt(..., SO_SNDBUF, %v,..) = %v", bufSize, err)
 			}
 
 			if !*swgso && *gso != 0 {
