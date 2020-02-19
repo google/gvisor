08a0a094abc3010b2990577fe849033bb5c1c7e5
Eyal Soha <eyalsoha@google.com>
2020-02-19 07:40:28 -0800

    Packetimpact in Go with c++ stub
    PiperOrigin-RevId: 295965583

---
+++ test/packetimpact/testbench/endian_unsafe.go
@@ -0,0 +1,37 @@
+// Copyright 2020 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testbench
+
+import (
+	"encoding/binary"
+	"unsafe"
+)
+
+func htons(x uint16) uint16 {
+	var nativeEndian binary.ByteOrder
+	buf := [2]byte{}
+	*(*uint16)(unsafe.Pointer(&buf[0])) = uint16(0xABCD)
+
+	switch buf {
+	case [2]byte{0xCD, 0xAB}:
+		nativeEndian = binary.LittleEndian
+	case [2]byte{0xAB, 0xCD}:
+		nativeEndian = binary.BigEndian
+	default:
+		panic("Could not determine native endianness.")
+	}
+	binary.BigEndian.PutUint16(buf[:], x)
+	return nativeEndian.Uint16(buf[:])
+}
