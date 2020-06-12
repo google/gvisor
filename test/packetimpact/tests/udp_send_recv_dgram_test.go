61b89be2326a1a70899f352aa5c47d272614bd9a
Tony Gong <gongt@google.com>
2020-06-12 15:06:01 -0700

    Make GenerateRandomPayload available to all tests
    Moved the function for generating a payload of random byets of a specified
    length into the testbench package so that it's availbale for all tests to use.
    
    Added a test case to the IPv4 ID uniqueness test which uses a payload length
    of 512 bytes. This test case passes for gVisor currently, whereas the test case
    with a small payload of 11 bytes fails because gVisor only assigns the ID field
    if the IP payload is sufficiently large.
    
    PiperOrigin-RevId: 316185097

--- test/packetimpact/tests/udp_send_recv_dgram_test.go
+++ test/packetimpact/tests/udp_send_recv_dgram_test.go
@@ -16,7 +16,6 @@ package udp_send_recv_dgram_test
 
 import (
 	"flag"
-	"math/rand"
 	"net"
 	"testing"
 	"time"
@@ -29,15 +28,6 @@ func init() {
 	testbench.RegisterFlags(flag.CommandLine)
 }
 
-func generateRandomPayload(t *testing.T, n int) string {
-	t.Helper()
-	buf := make([]byte, n)
-	if _, err := rand.Read(buf); err != nil {
-		t.Fatalf("rand.Read(buf) failed: %s", err)
-	}
-	return string(buf)
-}
-
 func TestUDPRecv(t *testing.T) {
 	dut := testbench.NewDUT(t)
 	defer dut.TearDown()
@@ -48,19 +38,19 @@ func TestUDPRecv(t *testing.T) {
 
 	testCases := []struct {
 		name    string
-		payload string
+		payload []byte
 	}{
-		{"emptypayload", ""},
-		{"small payload", "hello world"},
-		{"1kPayload", generateRandomPayload(t, 1<<10)},
+		{"emptypayload", nil},
+		{"small payload", []byte("hello world")},
+		{"1kPayload", testbench.GenerateRandomPayload(t, 1<<10)},
 		// Even though UDP allows larger dgrams we don't test it here as
 		// they need to be fragmented and written out as individual
 		// frames.
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			conn.Send(testbench.UDP{}, &testbench.Payload{Bytes: []byte(tc.payload)})
-			if got, want := string(dut.Recv(boundFD, int32(len(tc.payload)), 0)), tc.payload; got != want {
+			conn.Send(testbench.UDP{}, &testbench.Payload{Bytes: tc.payload})
+			if got, want := string(dut.Recv(boundFD, int32(len(tc.payload)), 0)), string(tc.payload); got != want {
 				t.Fatalf("received payload does not match sent payload got: %s, want: %s", got, want)
 			}
 		})
@@ -77,11 +67,11 @@ func TestUDPSend(t *testing.T) {
 
 	testCases := []struct {
 		name    string
-		payload string
+		payload []byte
 	}{
-		{"emptypayload", ""},
-		{"small payload", "hello world"},
-		{"1kPayload", generateRandomPayload(t, 1<<10)},
+		{"emptypayload", nil},
+		{"small payload", []byte("hello world")},
+		{"1kPayload", testbench.GenerateRandomPayload(t, 1<<10)},
 		// Even though UDP allows larger dgrams we don't test it here as
 		// they need to be fragmented and written out as individual
 		// frames.
@@ -89,10 +79,10 @@ func TestUDPSend(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			conn.Drain()
-			if got, want := int(dut.SendTo(boundFD, []byte(tc.payload), 0, conn.LocalAddr())), len(tc.payload); got != want {
+			if got, want := int(dut.SendTo(boundFD, tc.payload, 0, conn.LocalAddr())), len(tc.payload); got != want {
 				t.Fatalf("short write got: %d, want: %d", got, want)
 			}
-			if _, err := conn.ExpectData(testbench.UDP{SrcPort: &remotePort}, testbench.Payload{Bytes: []byte(tc.payload)}, 1*time.Second); err != nil {
+			if _, err := conn.ExpectData(testbench.UDP{SrcPort: &remotePort}, testbench.Payload{Bytes: tc.payload}, 1*time.Second); err != nil {
 				t.Fatal(err)
 			}
 		})
