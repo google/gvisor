b6e28912f1dfc1e4c2763e021a9870ba58556d08
Kevin Krakauer <krakauer@google.com>
2020-06-26 11:18:00 -0700

    Enable docker IPv6 in kokoro
    We need docker IPv6 for ip6tables tests. Docker has spotty IPv6 support, so just
    enabling IPv6 in daemon.json breaks the Ruby image test (AFAICT it breaks
    anything that tries to use IPv6 to reach the internet). An ip6tables NAT rule is
    added to handle this.
    
    We could make these changes as part of scripts/, but these would overwrite
    global values and possibly break users' systems.
    
    PiperOrigin-RevId: 318508209

--- tools/vm/ubuntu1604/25_docker.sh
+++ tools/vm/ubuntu1604/25_docker.sh
@@ -52,3 +52,14 @@ while true; do
     exit $result
   fi
 done
+
+# Enable Docker IPv6.
+cat > /etc/docker/daemon.json <<EOF
+{
+    "fixed-cidr-v6": "2001:db8:1::/64",
+    "ipv6": true
+}
+EOF
+# Docker's IPv6 support is lacking and does not work the same way as IPv4. We
+# can use NAT so containers can reach the outside world.
+ip6tables -t nat -A POSTROUTING -s 2001:db8:1::/64 ! -o docker0 -j MASQUERADE
