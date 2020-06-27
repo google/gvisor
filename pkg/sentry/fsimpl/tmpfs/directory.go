02d552d07c4415978d2ce418fb16baf238d0ff78
Dean Deng <deandeng@google.com>
2020-06-27 14:38:20 -0700

    Support sticky bit in vfs2.
    Updates #2923.
    
    PiperOrigin-RevId: 318648128

--- pkg/sentry/fsimpl/tmpfs/directory.go
+++ pkg/sentry/fsimpl/tmpfs/directory.go
@@ -81,6 +81,10 @@ func (dir *directory) removeChildLocked(child *dentry) {
 	dir.iterMu.Unlock()
 }
 
+func (dir *directory) mayDelete(creds *auth.Credentials, child *dentry) error {
+	return vfs.CheckDeleteSticky(creds, linux.FileMode(atomic.LoadUint32(&dir.inode.mode)), auth.KUID(atomic.LoadUint32(&child.inode.uid)))
+}
+
 type directoryFD struct {
 	fileDescription
 	vfs.DirectoryFileDescriptionDefaultImpl
