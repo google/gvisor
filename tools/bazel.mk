0db76ef49cac934cd6275e8ec36e4bedbdceef97
Kevin Krakauer <krakauer@google.com>
2020-07-22 12:04:33 -0700

    Unblock website building in kokoro
    Commands in the if;then must be grouped when on a single line. If this
    condition is hit, it may cause the script to fail.
    
    PiperOrigin-RevId: 322624898

--- tools/bazel.mk
+++ tools/bazel.mk
@@ -87,7 +87,7 @@ bazel-server-start: load-default ## Starts the bazel server.
 	               $(BAZEL) version && \
 		       exec tail --pid=\$$($(BAZEL) info server_pid) -f /dev/null"
 	@while :; do if docker logs $(DOCKER_NAME) 2>/dev/null | grep "Build label:" >/dev/null; then break; fi; \
-		if ! docker ps | grep $(DOCKER_NAME); then docker logs $(DOCKER_NAME); exit 1; else sleep 1; fi; done
+		if ! docker ps | grep $(DOCKER_NAME); then { docker logs $(DOCKER_NAME); exit 1; } else sleep 1; fi; done
 .PHONY: bazel-server-start
 
 bazel-shutdown: ## Shuts down a running bazel server.
