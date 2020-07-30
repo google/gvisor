9386cada4f6ee5dfaf80d08dd6d0067fc3a03bf7
Adin Scannell <ascannell@google.com>
2020-07-30 09:23:44 -0700

    Disable consistently failing test.
    PiperOrigin-RevId: 324017310

--- test/syscalls/linux/socket_inet_loopback_nogotsan.cc
+++ test/syscalls/linux/socket_inet_loopback_nogotsan.cc
@@ -82,8 +82,11 @@ using SocketInetLoopbackTest = ::testing::TestWithParam<TestParam>;
 
 // This test verifies that connect returns EADDRNOTAVAIL if all local ephemeral
 // ports are already in use for a given destination ip/port.
+//
 // We disable S/R because this test creates a large number of sockets.
-TEST_P(SocketInetLoopbackTest, TestTCPPortExhaustion_NoRandomSave) {
+//
+// FIXME(b/162475855): This test is failing reliably.
+TEST_P(SocketInetLoopbackTest, DISABLED_TestTCPPortExhaustion_NoRandomSave) {
   auto const& param = GetParam();
   TestAddress const& listener = param.listener;
   TestAddress const& connector = param.connector;
