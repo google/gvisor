1403a88c67ccc87af383f5e54fd2f77c673eebe7
Dean Deng <deandeng@google.com>
2020-08-05 16:14:40 -0700

    Release extra memfd reference.
    PiperOrigin-RevId: 325122849

--- pkg/sentry/syscalls/linux/vfs2/memfd.go
+++ pkg/sentry/syscalls/linux/vfs2/memfd.go
@@ -51,6 +51,7 @@ func MemfdCreate(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.S
 	if err != nil {
 		return 0, nil, err
 	}
+	defer file.DecRef(t)
 
 	fd, err := t.NewFDFromVFS2(0, file, kernel.FDFlags{
 		CloseOnExec: cloExec,
