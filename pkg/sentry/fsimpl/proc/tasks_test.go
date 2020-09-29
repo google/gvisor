64861cca516e6d28237838486f8593314a6d620d
Fabricio Voznika <fvoznika@google.com>
2020-09-29 15:47:25 -0700

    Add /proc/[pid]/cwd
    PiperOrigin-RevId: 334478850

--- pkg/sentry/fsimpl/proc/tasks_test.go
+++ pkg/sentry/fsimpl/proc/tasks_test.go
@@ -67,6 +67,7 @@ var (
 	taskStaticFiles = map[string]testutil.DirentType{
 		"auxv":          linux.DT_REG,
 		"cgroup":        linux.DT_REG,
+		"cwd":           linux.DT_LNK,
 		"cmdline":       linux.DT_REG,
 		"comm":          linux.DT_REG,
 		"environ":       linux.DT_REG,
