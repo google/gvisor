a21f76562764b6d2c8a2214031c3bc9357315092
Arthur Sfez <asfez@google.com>
2020-10-29 16:20:44 -0700

    Add IPv4 reassembly packetimpact test
    The IPv6 reassembly test was also refactored to be easily extended with
    more cases.
    
    PiperOrigin-RevId: 339768605

--- test/packetimpact/tests/ipv4_fragment_reassembly_test.go
+++ test/packetimpact/tests/ipv4_fragment_reassembly_test.go
@@ -12,17 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ipv6_fragment_reassembly_test
+package ipv4_fragment_reassembly_test
 
 import (
 	"flag"
 	"math/rand"
-	"net"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
-	"gvisor.dev/gvisor/pkg/tcpip"
 	"gvisor.dev/gvisor/pkg/tcpip/buffer"
 	"gvisor.dev/gvisor/pkg/tcpip/header"
 	"gvisor.dev/gvisor/test/packetimpact/testbench"
@@ -35,12 +33,12 @@ func init() {
 type fragmentInfo struct {
 	offset uint16
 	size   uint16
-	more   bool
+	more   uint8
 }
 
-func TestIPv6FragmentReassembly(t *testing.T) {
+func TestIPv4FragmentReassembly(t *testing.T) {
 	const fragmentID = 42
-	icmpv6ProtoNum := header.IPv6ExtensionHeaderIdentifier(header.ICMPv6ProtocolNumber)
+	icmpv4ProtoNum := uint8(header.ICMPv4ProtocolNumber)
 
 	tests := []struct {
 		description  string
@@ -50,20 +48,19 @@ func TestIPv6FragmentReassembly(t *testing.T) {
 	}{
 		{
 			description:  "basic reassembly",
-			ipPayloadLen: 1500,
+			ipPayloadLen: 2000,
 			fragments: []fragmentInfo{
-				{offset: 0, size: 760, more: true},
-				{offset: 760, size: 740, more: false},
+				{offset: 0, size: 1000, more: header.IPv4FlagMoreFragments},
+				{offset: 1000, size: 1000, more: 0},
 			},
 			expectReply: true,
 		},
 		{
 			description:  "out of order fragments",
-			ipPayloadLen: 3000,
+			ipPayloadLen: 2000,
 			fragments: []fragmentInfo{
-				{offset: 0, size: 1024, more: true},
-				{offset: 2048, size: 952, more: false},
-				{offset: 1024, size: 1024, more: true},
+				{offset: 1000, size: 1000, more: 0},
+				{offset: 0, size: 1000, more: header.IPv4FlagMoreFragments},
 			},
 			expectReply: true,
 		},
@@ -73,37 +70,33 @@ func TestIPv6FragmentReassembly(t *testing.T) {
 		t.Run(test.description, func(t *testing.T) {
 			dut := testbench.NewDUT(t)
 			defer dut.TearDown()
-			conn := testbench.NewIPv6Conn(t, testbench.IPv6{}, testbench.IPv6{})
+			conn := testbench.NewIPv4Conn(t, testbench.IPv4{}, testbench.IPv4{})
 			defer conn.Close(t)
 
-			lIP := tcpip.Address(net.ParseIP(testbench.LocalIPv6).To16())
-			rIP := tcpip.Address(net.ParseIP(testbench.RemoteIPv6).To16())
-
 			data := make([]byte, test.ipPayloadLen)
-			icmp := header.ICMPv6(data[:header.ICMPv6HeaderSize])
-			icmp.SetType(header.ICMPv6EchoRequest)
-			icmp.SetCode(header.ICMPv6UnusedCode)
+			icmp := header.ICMPv4(data[:header.ICMPv4MinimumSize])
+			icmp.SetType(header.ICMPv4Echo)
+			icmp.SetCode(header.ICMPv4UnusedCode)
 			icmp.SetChecksum(0)
-			originalPayload := data[header.ICMPv6HeaderSize:]
+			icmp.SetSequence(0)
+			icmp.SetIdent(0)
+			originalPayload := data[header.ICMPv4MinimumSize:]
 			if _, err := rand.Read(originalPayload); err != nil {
 				t.Fatalf("rand.Read: %s", err)
 			}
-
-			cksum := header.ICMPv6Checksum(
+			cksum := header.ICMPv4Checksum(
 				icmp,
-				lIP,
-				rIP,
 				buffer.NewVectorisedView(len(originalPayload), []buffer.View{originalPayload}),
 			)
 			icmp.SetChecksum(cksum)
 
 			for _, fragment := range test.fragments {
-				conn.Send(t, testbench.IPv6{},
-					&testbench.IPv6FragmentExtHdr{
-						NextHeader:     &icmpv6ProtoNum,
-						FragmentOffset: testbench.Uint16(fragment.offset / header.IPv6FragmentExtHdrFragmentOffsetBytesPerUnit),
-						MoreFragments:  testbench.Bool(fragment.more),
-						Identification: testbench.Uint32(fragmentID),
+				conn.Send(t,
+					testbench.IPv4{
+						Protocol:       &icmpv4ProtoNum,
+						FragmentOffset: testbench.Uint16(fragment.offset),
+						Flags:          testbench.Uint8(fragment.more),
+						ID:             testbench.Uint16(fragmentID),
 					},
 					&testbench.Payload{
 						Bytes: data[fragment.offset:][:fragment.size],
@@ -115,9 +108,8 @@ func TestIPv6FragmentReassembly(t *testing.T) {
 			for {
 				incomingFrame, err := conn.ExpectFrame(t, testbench.Layers{
 					&testbench.Ether{},
-					&testbench.IPv6{},
-					&testbench.IPv6FragmentExtHdr{},
-					&testbench.ICMPv6{},
+					&testbench.IPv4{},
+					&testbench.ICMPv4{},
 				}, time.Second)
 				if err != nil {
 					// Either an unexpected frame was received, or none at all.
@@ -129,12 +121,11 @@ func TestIPv6FragmentReassembly(t *testing.T) {
 				if !test.expectReply {
 					t.Fatalf("unexpected reply received:\n%s", incomingFrame)
 				}
-				ipPayload, err := incomingFrame[3 /* ICMPv6 */].ToBytes()
+				ipPayload, err := incomingFrame[2 /* ICMPv4 */].ToBytes()
 				if err != nil {
-					t.Fatalf("failed to parse ICMPv6 header: incomingPacket[3].ToBytes() = (_, %s)", err)
+					t.Fatalf("failed to parse ICMPv4 header: incomingPacket[2].ToBytes() = (_, %s)", err)
 				}
-				offset := *incomingFrame[2 /* IPv6FragmentExtHdr */].(*testbench.IPv6FragmentExtHdr).FragmentOffset
-				offset *= header.IPv6FragmentExtHdrFragmentOffsetBytesPerUnit
+				offset := *incomingFrame[1 /* IPv4 */].(*testbench.IPv4).FragmentOffset
 				if copied := copy(reassembledPayload[offset:], ipPayload); copied != len(ipPayload) {
 					t.Fatalf("wrong number of bytes copied into reassembledPayload: got = %d, want = %d", copied, len(ipPayload))
 				}
@@ -142,7 +133,7 @@ func TestIPv6FragmentReassembly(t *testing.T) {
 			}
 
 			if test.expectReply {
-				if diff := cmp.Diff(originalPayload, reassembledPayload[header.ICMPv6HeaderSize:]); diff != "" {
+				if diff := cmp.Diff(originalPayload, reassembledPayload[header.ICMPv4MinimumSize:]); diff != "" {
 					t.Fatalf("reassembledPayload mismatch (-want +got):\n%s", diff)
 				}
 			}
