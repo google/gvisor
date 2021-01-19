a2ec1932c9b76d80d3af93aa74acf945829cd582
Ghanan Gowripalan <ghanan@google.com>
2021-01-19 12:06:08 -0800

    Drop CheckLocalAddress from LinkAddressCache
    PiperOrigin-RevId: 352623277

--- pkg/tcpip/network/arp/arp.go
+++ pkg/tcpip/network/arp/arp.go
@@ -138,23 +138,17 @@ func (e *endpoint) HandlePacket(pkt *stack.PacketBuffer) {
 		stats.RequestsReceived.Increment()
 		localAddr := tcpip.Address(h.ProtocolAddressTarget())
 
+		if e.protocol.stack.CheckLocalAddress(e.nic.ID(), header.IPv4ProtocolNumber, localAddr) == 0 {
+			stats.RequestsReceivedUnknownTargetAddress.Increment()
+			return // we have no useful answer, ignore the request
+		}
+
+		remoteAddr := tcpip.Address(h.ProtocolAddressSender())
+		remoteLinkAddr := tcpip.LinkAddress(h.HardwareAddressSender())
+
 		if e.nud == nil {
-			if e.linkAddrCache.CheckLocalAddress(e.nic.ID(), header.IPv4ProtocolNumber, localAddr) == 0 {
-				stats.RequestsReceivedUnknownTargetAddress.Increment()
-				return // we have no useful answer, ignore the request
-			}
-
-			addr := tcpip.Address(h.ProtocolAddressSender())
-			linkAddr := tcpip.LinkAddress(h.HardwareAddressSender())
-			e.linkAddrCache.AddLinkAddress(e.nic.ID(), addr, linkAddr)
+			e.linkAddrCache.AddLinkAddress(e.nic.ID(), remoteAddr, remoteLinkAddr)
 		} else {
-			if e.protocol.stack.CheckLocalAddress(e.nic.ID(), header.IPv4ProtocolNumber, localAddr) == 0 {
-				stats.RequestsReceivedUnknownTargetAddress.Increment()
-				return // we have no useful answer, ignore the request
-			}
-
-			remoteAddr := tcpip.Address(h.ProtocolAddressSender())
-			remoteLinkAddr := tcpip.LinkAddress(h.HardwareAddressSender())
 			e.nud.HandleProbe(remoteAddr, ProtocolNumber, remoteLinkAddr, e.protocol)
 		}
 
