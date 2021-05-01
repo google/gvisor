08abd90a6ad08ec52ab7db41d2071e77644d3a9d
Ghanan Gowripalan <ghanan@google.com>
2021-04-30 17:03:25 -0700

    Comment ip package in a single place
    Fixes the below linting error:
    ```
      From Golint:
      > Package ip has package comment defined in multiple places:
      >   duplicate_address_detection.go
      >   generic_multicast_protocol.go
    ```
    
    PiperOrigin-RevId: 371430486

--- pkg/tcpip/network/internal/ip/generic_multicast_protocol.go
+++ pkg/tcpip/network/internal/ip/generic_multicast_protocol.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package ip holds IPv4/IPv6 common utilities.
 package ip
 
 import (
