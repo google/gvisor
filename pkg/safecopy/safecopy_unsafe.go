d1ab856135fce5361fc3d1fd0d42c1318cfccd3d
Ian Lewis <ianlewis@google.com>
2021-06-13 19:21:46 -0700

    Remove usermem dependency from marshal
    Both marshal and usermem are depended on by many packages and a dependency on
    marshal can often create circular dependencies. marshal should consider adding
    internal dependencies carefully moving forward.
    
    Fixes #6160
    
    PiperOrigin-RevId: 379199882

--- pkg/safecopy/safecopy_unsafe.go
+++ pkg/safecopy/safecopy_unsafe.go
@@ -20,6 +20,7 @@ import (
 	"unsafe"
 
 	"golang.org/x/sys/unix"
+	"gvisor.dev/gvisor/pkg/abi/linux"
 )
 
 // maxRegisterSize is the maximum register size used in memcpy and memclr. It
@@ -342,15 +343,7 @@ func errorFromFaultSignal(addr uintptr, sig int32) error {
 // handler however, and if this is function is being used externally then the
 // same courtesy is expected.
 func ReplaceSignalHandler(sig unix.Signal, handler uintptr, previous *uintptr) error {
-	// TODO(gvisor.dev/issue/6160): This struct is the same as linux.SigAction.
-	// Once the usermem dependency is removed from primitive, delete this replica
-	// and remove IFTTT comments in abi/linux/signal.go.
-	var sa struct {
-		handler  uintptr
-		flags    uint64
-		restorer uintptr
-		mask     uint64
-	}
+	var sa linux.SigAction
 	const maskLen = 8
 
 	// Get the existing signal handler information, and save the current
@@ -361,14 +354,14 @@ func ReplaceSignalHandler(sig unix.Signal, handler uintptr, previous *uintptr) e
 	}
 
 	// Fail if there isn't a previous handler.
-	if sa.handler == 0 {
+	if sa.Handler == 0 {
 		return fmt.Errorf("previous handler for signal %x isn't set", sig)
 	}
 
-	*previous = sa.handler
+	*previous = uintptr(sa.Handler)
 
 	// Install our own handler.
-	sa.handler = handler
+	sa.Handler = uint64(handler)
 	if _, _, e := unix.RawSyscall6(unix.SYS_RT_SIGACTION, uintptr(sig), uintptr(unsafe.Pointer(&sa)), 0, maskLen, 0, 0); e != 0 {
 		return e
 	}
