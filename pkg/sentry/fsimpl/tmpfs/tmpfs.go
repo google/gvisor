aa9b4c1dbbccfdd3107631a5e7c5106bbccc4dd7
Andrei Vagin <avagin@google.com>
2021-07-20 17:46:36 -0700

    fs/tmpfs: inode.mode must be accessed atomically.
    Reported-by: syzbot+59550b48e06cc0d3b638@syzkaller.appspotmail.com
    PiperOrigin-RevId: 385908873

--- pkg/sentry/fsimpl/tmpfs/tmpfs.go
+++ pkg/sentry/fsimpl/tmpfs/tmpfs.go
@@ -397,10 +397,13 @@ func (i *inode) init(impl interface{}, fs *filesystem, kuid auth.KUID, kgid auth
 	}
 
 	// Inherit the group and setgid bit as in fs/inode.c:inode_init_owner().
-	if parentDir != nil && parentDir.inode.mode&linux.S_ISGID == linux.S_ISGID {
-		kgid = auth.KGID(parentDir.inode.gid)
-		if mode&linux.S_IFDIR == linux.S_IFDIR {
-			mode |= linux.S_ISGID
+	if parentDir != nil {
+		parentMode := atomic.LoadUint32(&parentDir.inode.mode)
+		if parentMode&linux.S_ISGID == linux.S_ISGID {
+			kgid = auth.KGID(parentDir.inode.gid)
+			if mode&linux.S_IFDIR == linux.S_IFDIR {
+				mode |= linux.S_ISGID
+			}
 		}
 	}
 
