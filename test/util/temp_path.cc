4f87185943e845aed5f8b9be6cda2e81cd8f4359
Zyad A. Ali <zyad.ali.me@gmail.com>
2021-10-05 11:41:12 -0700

    Implement mq_open(2) and mq_unlink(2).
    This PR is broken into two pieces
    - Implement `mqfs`, a filesystem used by POSIX message queues.
    - Implement `mq_open(2)` and `mq_unlink(2)` to support creation and deletion of POSIX message queues.
    
    Updates #136
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/gvisor/pull/6345 from sudo-sturbia:mq/syscalls f03dc73f0f46d0ff1ae209fefc98ee3d7fc725d2
    PiperOrigin-RevId: 401038104

--- test/util/temp_path.cc
+++ test/util/temp_path.cc
@@ -34,20 +34,6 @@ namespace {
 
 std::atomic<uint64_t> global_temp_file_number = ATOMIC_VAR_INIT(1);
 
-// Return a new temp filename, intended to be unique system-wide.
-//
-// The global file number helps maintain file naming consistency across
-// different runs of a test.
-//
-// The timestamp is necessary because the test infrastructure invokes each
-// test case in a separate process (resetting global_temp_file_number) and
-// potentially in parallel, which allows for races between selecting and using a
-// name.
-std::string NextTempBasename() {
-  return absl::StrCat("gvisor_test_temp_", global_temp_file_number++, "_",
-                      absl::ToUnixNanos(absl::Now()));
-}
-
 void TryDeleteRecursively(std::string const& path) {
   if (!path.empty()) {
     int undeleted_dirs = 0;
@@ -83,6 +69,18 @@ std::string GetAbsoluteTestTmpdir() {
       env_tmpdir != nullptr ? std::string(env_tmpdir) : "/tmp";
 
   return MakeAbsolute(tmp_dir, "").ValueOrDie();
+}
+
+// The global file number helps maintain file naming consistency across
+// different runs of a test.
+//
+// The timestamp is necessary because the test infrastructure invokes each
+// test case in a separate process (resetting global_temp_file_number) and
+// potentially in parallel, which allows for races between selecting and using a
+// name.
+std::string NextTempBasename() {
+  return absl::StrCat("gvisor_test_temp_", global_temp_file_number++, "_",
+                      absl::ToUnixNanos(absl::Now()));
 }
 
 PosixErrorOr<TempPath> TempPath::CreateFileWith(absl::string_view const parent,
