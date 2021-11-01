9776edb3fa9e67a28182aedc9342ef9c1b556d7c
Adin Scannell <ascannell@google.com>
2021-11-01 16:05:18 -0700

    Move ThreadGroupIDFromContext to kernel/auth.
    This function doesn't belong in the global context package. Move to a more
    suitable package to break the dependency cycle.
    
    PiperOrigin-RevId: 406942122

--- pkg/sentry/kernel/auth/context.go
+++ pkg/sentry/kernel/auth/context.go
@@ -24,6 +24,10 @@ type contextID int
 const (
 	// CtxCredentials is a Context.Value key for Credentials.
 	CtxCredentials contextID = iota
+
+	// CtxThreadGroupID is the current thread group ID when a context represents
+	// a task context. The value is represented as an int32.
+	CtxThreadGroupID contextID = iota
 )
 
 // CredentialsFromContext returns a copy of the Credentials used by ctx, or a
@@ -33,6 +37,15 @@ func CredentialsFromContext(ctx context.Context) *Credentials {
 		return v.(*Credentials)
 	}
 	return NewAnonymousCredentials()
+}
+
+// ThreadGroupIDFromContext returns the current thread group ID when ctx
+// represents a task context.
+func ThreadGroupIDFromContext(ctx context.Context) (tgid int32, ok bool) {
+	if tgid := ctx.Value(CtxThreadGroupID); tgid != nil {
+		return tgid.(int32), true
+	}
+	return 0, false
 }
 
 // ContextWithCredentials returns a copy of ctx carrying creds.
