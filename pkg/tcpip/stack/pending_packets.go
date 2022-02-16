0917380792ac17eeb27784104bc46704ce7bc126
Bhasker Hariharan <bhaskerh@google.com>
2022-02-15 18:26:42 -0800

    Clear pending packet queue when nic is removed.
    Updates #6910
    
    PiperOrigin-RevId: 428925334

--- pkg/tcpip/stack/pending_packets.go
+++ pkg/tcpip/stack/pending_packets.go
@@ -70,6 +70,20 @@ func (f *packetsPendingLinkResolution) init(nic *nic) {
 	f.mu.packets = make(map[<-chan struct{}][]pendingPacket)
 }
 
+// cancel drains all pending packet queues and release all packet
+// references.
+func (f *packetsPendingLinkResolution) cancel() {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	for ch, pendingPackets := range f.mu.packets {
+		for _, p := range pendingPackets {
+			p.pkt.DecRef()
+		}
+		delete(f.mu.packets, ch)
+	}
+	f.mu.cancelChans = nil
+}
+
 // dequeue any pending packets associated with ch.
 //
 // If err is nil, packets will be written and sent to the given remote link
