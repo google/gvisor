76629b23847097ec2f6b66a2a944db8723623b0c
Andrei Vagin <avagin@google.com>
2022-03-22 22:09:21 -0700

    Remove unused ring0.RestoreKernelFPState.
    PiperOrigin-RevId: 436650028

--- pkg/ring0/kernel_arm64.go
+++ pkg/ring0/kernel_arm64.go
@@ -90,9 +90,3 @@ func (c *CPU) SwitchToUser(switchOpts SwitchOpts) (vector Vector) {
 
 	return
 }
-
-// RestoreKernelFPState restores the Sentry floating point state.
-//
-//go:nosplit
-func RestoreKernelFPState() {
-}
