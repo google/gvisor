4ca36a9e607cb851b5fecd5455358484fdc7d7d0
Ghanan Gowripalan <ghanan@google.com>
2022-05-05 12:50:29 -0700

    Respect SO_SNDBUF for network datagram endpoints
    Previously, SO_SNDBUF was effectively a no-op. The stack should make
    sure that only SO_SNDBUF bytes are ever in-flight for any given
    socket/endpoint.
    
    Fuchsia Bug: https://fxbug.dev/99070
    
    PiperOrigin-RevId: 446792223

--- pkg/tcpip/transport/datagram_test.go
+++ pkg/tcpip/transport/datagram_test.go
@@ -16,10 +16,13 @@
 package transport_test
 
 import (
+	"bytes"
 	"fmt"
+	"math"
 	"testing"
 
 	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip/buffer"
 	"gvisor.dev/gvisor/pkg/tcpip/header"
 	"gvisor.dev/gvisor/pkg/tcpip/link/loopback"
 	"gvisor.dev/gvisor/pkg/tcpip/network/ipv4"
@@ -123,6 +126,225 @@ func TestStateUpdates(t *testing.T) {
 			if got, want := transport.DatagramEndpointState(ep.State()), transport.DatagramEndpointStateClosed; got != want {
 				t.Errorf("got ep.State() = %s, want = %s", got, want)
 			}
+		})
+	}
+}
+
+type mockEndpoint struct {
+	disp stack.NetworkDispatcher
+	pkts stack.PacketBufferList
+}
+
+func (*mockEndpoint) MTU() uint32 {
+	return math.MaxUint32
+}
+func (*mockEndpoint) Capabilities() stack.LinkEndpointCapabilities {
+	return 0
+}
+func (*mockEndpoint) MaxHeaderLength() uint16 {
+	return 0
+}
+func (*mockEndpoint) LinkAddress() tcpip.LinkAddress {
+	var l tcpip.LinkAddress
+	return l
+}
+func (e *mockEndpoint) WritePackets(pkts stack.PacketBufferList) (int, tcpip.Error) {
+	pkts.IncRef()
+	len := pkts.Len()
+	for pkt := pkts.Front(); pkt != nil; pkt = pkt.Next() {
+		e.pkts.PushBack(pkt)
+	}
+
+	return len, nil
+}
+func (e *mockEndpoint) Attach(d stack.NetworkDispatcher)      { e.disp = d }
+func (e *mockEndpoint) IsAttached() bool                      { return e.disp != nil }
+func (*mockEndpoint) Wait()                                   {}
+func (*mockEndpoint) ARPHardwareType() header.ARPHardwareType { return header.ARPHardwareNone }
+func (*mockEndpoint) AddHeader(*stack.PacketBuffer)           {}
+func (e *mockEndpoint) releasePackets() {
+	e.pkts.DecRef()
+	e.pkts = stack.PacketBufferList{}
+}
+
+func (e *mockEndpoint) pktsSize() int {
+	s := 0
+	for pkt := e.pkts.Front(); pkt != nil; pkt = pkt.Next() {
+		s += pkt.Size() + pkt.AvailableHeaderBytes()
+	}
+	return s
+}
+
+func TestSndBuf(t *testing.T) {
+	const nicID = 1
+
+	buf := buffer.NewView(header.ICMPv4MinimumSize)
+	header.ICMPv4(buf).SetType(header.ICMPv4Echo)
+
+	for _, test := range []struct {
+		name           string
+		createEndpoint func(*stack.Stack, *waiter.Queue) (tcpip.Endpoint, error)
+	}{
+		{
+			name: "UDP",
+			createEndpoint: func(s *stack.Stack, wq *waiter.Queue) (tcpip.Endpoint, error) {
+				ep, err := s.NewEndpoint(udp.ProtocolNumber, ipv4.ProtocolNumber, wq)
+				if err != nil {
+					return nil, fmt.Errorf("s.NewEndpoint(%d, %d, _) failed: %s", udp.ProtocolNumber, ipv4.ProtocolNumber, err)
+				}
+				return ep, nil
+			},
+		},
+		{
+			name: "ICMP",
+			createEndpoint: func(s *stack.Stack, wq *waiter.Queue) (tcpip.Endpoint, error) {
+				ep, err := s.NewEndpoint(icmp.ProtocolNumber4, ipv4.ProtocolNumber, wq)
+				if err != nil {
+					return nil, fmt.Errorf("s.NewEndpoint(%d, %d, _) failed: %s", icmp.ProtocolNumber4, ipv4.ProtocolNumber, err)
+				}
+				return ep, nil
+			},
+		},
+		{
+			name: "RAW",
+			createEndpoint: func(s *stack.Stack, wq *waiter.Queue) (tcpip.Endpoint, error) {
+				ep, err := s.NewRawEndpoint(udp.ProtocolNumber, ipv4.ProtocolNumber, wq, true /* associated */)
+				if err != nil {
+					return nil, fmt.Errorf("s.NewRawEndpoint(%d, %d, _, true) failed: %s", udp.ProtocolNumber, ipv4.ProtocolNumber, err)
+				}
+				return ep, nil
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			s := stack.New(stack.Options{
+				NetworkProtocols:   []stack.NetworkProtocolFactory{ipv4.NewProtocol},
+				TransportProtocols: []stack.TransportProtocolFactory{udp.NewProtocol, icmp.NewProtocol4},
+				RawFactory:         &raw.EndpointFactory{},
+			})
+			var e mockEndpoint
+			defer e.releasePackets()
+			if err := s.CreateNIC(nicID, &e); err != nil {
+				t.Fatalf("s.CreateNIC(%d, _) failed: %s", nicID, err)
+			}
+			var wq waiter.Queue
+			ep, err := test.createEndpoint(s, &wq)
+			if err != nil {
+				t.Fatalf("test.createEndpoint(_) failed: %s", err)
+			}
+			defer ep.Close()
+
+			addr := tcpip.ProtocolAddress{
+				Protocol:          ipv4.ProtocolNumber,
+				AddressWithPrefix: testutil.MustParse4("1.2.3.4").WithPrefix(),
+			}
+			if err := s.AddProtocolAddress(nicID, addr, stack.AddressProperties{}); err != nil {
+				t.Fatalf("AddProtocolAddress(%d, %#v, {}): %s", nicID, addr, err)
+			}
+			s.SetRouteTable([]tcpip.Route{
+				{
+					Destination: header.IPv4EmptySubnet,
+					NIC:         nicID,
+				},
+			})
+
+			to := tcpip.FullAddress{NIC: nicID, Addr: testutil.MustParse4("1.0.0.1"), Port: 12345}
+			if err := ep.Connect(to); err != nil {
+				t.Fatalf("ep.Connect(%#v): %s", to, err)
+			}
+
+			checkWriteFail := func() {
+				t.Helper()
+
+				if got := ep.Readiness(waiter.WritableEvents); got != 0 {
+					t.Fatalf("got ep.Readiness(0x%x) = 0x%x, want = 0x0", waiter.WritableEvents, got)
+				}
+
+				var r bytes.Reader
+				r.Reset(buf[:])
+				wantErr := &tcpip.ErrWouldBlock{}
+				if n, err := ep.Write(&r, tcpip.WriteOptions{}); err != wantErr {
+					t.Fatalf("got Write(...) = (%d, %s), want = (_, %s)", n, err, wantErr)
+				}
+
+			}
+
+			checkWrites := func() {
+				t.Helper()
+
+				if got := ep.Readiness(waiter.WritableEvents); got != waiter.WritableEvents {
+					t.Fatalf("got ep.Readiness(0x%x) = 0x%x, want = 0x%x", waiter.WritableEvents, got, waiter.WritableEvents)
+				}
+
+				var r bytes.Reader
+				r.Reset(buf[:])
+				if n, err := ep.Write(&r, tcpip.WriteOptions{}); err != nil {
+					t.Fatalf("Write(...): %s", err)
+				} else if want := int64(len(buf)); n != want {
+					t.Fatalf("got Write(...) = %d, want = %d", n, want)
+				}
+
+				// The next write should fail since the packet we sent before
+				// is still held.
+				checkWriteFail()
+			}
+
+			we, ch := waiter.NewChannelEntry(waiter.WritableEvents)
+			wq.EventRegister(&we)
+			defer wq.EventUnregister(&we)
+
+			checkNoWritableEvent := func() {
+				t.Helper()
+
+				select {
+				case <-ch:
+					t.Fatal("unexpected writable event")
+				default:
+				}
+			}
+
+			checkWritableEvent := func() {
+				t.Helper()
+
+				select {
+				case <-ch:
+				default:
+					t.Fatal("expected writable event")
+				}
+			}
+
+			// As long as there is space in the send buffer, writes should succeed
+			// so a send buffer of 1 allows at max 1 in-flight packet.
+			ep.SocketOptions().SetSendBufferSize(1, true)
+			checkWritableEvent()
+			checkWrites()
+			checkNoWritableEvent()
+
+			// Increase the size of the send buffer but still be full.
+			inUseSize := int64(e.pktsSize())
+			checkNoWritableEvent()
+			ep.SocketOptions().SetSendBufferSize(inUseSize, true /* notify */)
+			checkNoWritableEvent()
+			checkWriteFail()
+
+			// Open up the send buffer by 1 byte.
+			checkNoWritableEvent()
+			ep.SocketOptions().SetSendBufferSize(inUseSize+1, true /* notify */)
+			checkWritableEvent()
+			checkWrites()
+
+			// We can resize the send buffer to a smaller size but it is still
+			// full so we can't write.
+			checkNoWritableEvent()
+			ep.SocketOptions().SetSendBufferSize(1, true /* notify */)
+			checkNoWritableEvent()
+			checkWriteFail()
+
+			// Releasing the packets should open up the send buffer for the next
+			// write.
+			e.releasePackets()
+			checkWritableEvent()
+			checkWrites()
 		})
 	}
 }
