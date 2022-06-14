fa7c3d9c5142a3b9e05bf64a2e60624e5e6ff5cf
Ghanan Gowripalan <ghanan@google.com>
2022-06-14 10:32:49 -0700

    Support specifying local IPv6 adddress/interface
    ...for outgoing packets.
    
    This is the first step in supporting IPV6_PKTINFO in the send path.
    
    Note that the implementation enforces strong host semantics as gVisor
    currently follows the strong host model.
    
    This change does not support IPV6_PKTINFO in the send path from Linux
    applications (via CMSGS) yet as there is no need. The bindings to enable
    the IPV6_PKTINFO CMSG in the sendpath can be done in a followup.
    
    Bug: https://fxbug.dev/102222
    PiperOrigin-RevId: 454898862

--- pkg/tcpip/transport/datagram_test.go
+++ pkg/tcpip/transport/datagram_test.go
@@ -23,7 +23,9 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip/checker"
 	"gvisor.dev/gvisor/pkg/tcpip/header"
+	"gvisor.dev/gvisor/pkg/tcpip/link/channel"
 	"gvisor.dev/gvisor/pkg/tcpip/link/loopback"
 	"gvisor.dev/gvisor/pkg/tcpip/network/ipv4"
 	"gvisor.dev/gvisor/pkg/tcpip/network/ipv6"
@@ -659,6 +661,454 @@ func TestMulticastLoop(t *testing.T) {
 
 					ops.SetMulticastLoop(true)
 					checkWrite([]byte{9, 10, 11, 12}, true /* withRead */)
+				})
+			}
+		})
+	}
+}
+
+func TestIPv6PacketInfo(t *testing.T) {
+	const (
+		nicID1 = 1
+		nicID2 = 2
+		port   = 12345
+	)
+
+	type localNICAddr struct {
+		nicID tcpip.NICID
+		addr  tcpip.AddressWithPrefix
+	}
+
+	type testCase struct {
+		name        string
+		boundNICID  tcpip.NICID
+		bindAddr    tcpip.FullAddress
+		connectAddr tcpip.FullAddress
+		toAddr      tcpip.FullAddress
+		pktInfo     tcpip.IPv6PacketInfo
+
+		expectedErr        tcpip.Error
+		expectedLocalAddr  tcpip.Address
+		expectedRemoteAddr tcpip.Address
+	}
+
+	ipv6Addr1 := testutil.MustParse6("1::1")
+	ipv6Addr2 := testutil.MustParse6("1::2")
+	ipv6RemoteAddr1 := testutil.MustParse6("2::1")
+	ipv6RemoteAddr2 := testutil.MustParse6("2::2")
+
+	localAddrs := []localNICAddr{
+		{
+			nicID: nicID1,
+			addr:  ipv6Addr1.WithPrefix(),
+		},
+		{
+			nicID: nicID2,
+			addr:  ipv6Addr2.WithPrefix(),
+		},
+	}
+
+	tests := []testCase{
+		// Bind and SendTo
+		{
+			name: "Bind wildcard & SendTo with packet info NIC",
+			bindAddr: tcpip.FullAddress{
+				Addr: "",
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+		{
+			name:       "BindToDevice & Bind wildcard & SendTo with packet info NIC not matching",
+			boundNICID: nicID2,
+			bindAddr: tcpip.FullAddress{
+				Addr: "",
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+		{
+			name: "Bind wildcard and NIC & SendTo with packet info NIC matching",
+			bindAddr: tcpip.FullAddress{
+				NIC:  nicID1,
+				Addr: "",
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+		{
+			name: "Bind wildcard and NIC & SendTo with packet info NIC not matching",
+			bindAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: "",
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+		{
+			name: "Bind specified & SendTo with packet info NIC not matching bound addr",
+			bindAddr: tcpip.FullAddress{
+				Addr: ipv6Addr2,
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrBadLocalAddress{},
+		},
+		{
+			name: "Bind specified and NIC & SendTo with packet info NIC not matching but local addr specified",
+			bindAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: ipv6Addr2,
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC:  nicID1,
+				Addr: ipv6Addr1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+
+		// Bind and Connect
+		{
+			name: "Bind wildcard & Connect then Send with packet info NIC",
+			bindAddr: tcpip.FullAddress{
+				Addr: "",
+				Port: port,
+			},
+			connectAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+		{
+			name: "Bind wildcard and NIC & Connect then Send with packet info NIC matching",
+			bindAddr: tcpip.FullAddress{
+				NIC:  nicID1,
+				Addr: "",
+				Port: port,
+			},
+			connectAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+		{
+			name: "Bind wildcard and NIC & Connect then Send with packet info NIC not matching",
+			bindAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: "",
+				Port: port,
+			},
+			connectAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+		{
+			name: "Bind wildcard & Connect with NIC then Send with packet info NIC matching",
+			bindAddr: tcpip.FullAddress{
+				Addr: "",
+				Port: port,
+			},
+			connectAddr: tcpip.FullAddress{
+				NIC:  nicID1,
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+		{
+			name: "Bind wildcard & Connect with NIC then Send with packet info NIC not matching",
+			bindAddr: tcpip.FullAddress{
+				Addr: "",
+				Port: port,
+			},
+			connectAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+		{
+			name: "Bind specified & Connect then Send with packet info NIC not matching but local addr specified",
+			bindAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: ipv6Addr2,
+				Port: port,
+			},
+			connectAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC:  nicID1,
+				Addr: ipv6Addr1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+
+		// Connect
+		{
+			name: "Connect with NIC then Send with packet info NIC matching",
+			connectAddr: tcpip.FullAddress{
+				NIC:  nicID1,
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+		{
+			// Because NIC2 is preferred over NIC1 for route selection, we pick a
+			// local address on NIC2. Since the pktinfo does not specify a local
+			// address but requests the packet to be sent out through NIC1 we fail
+			// with err bad local address because NIC2's local address is not
+			// available on NIC1.
+			name: "Connect then Send with packet info NIC not matching",
+			connectAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrBadLocalAddress{},
+		},
+		{
+			name:       "BindToDevice & Connect then Send with packet info NIC matching",
+			boundNICID: nicID2,
+			connectAddr: tcpip.FullAddress{
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+		{
+			name: "Connect then Send with packet info NIC not matching",
+			connectAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				NIC: nicID1,
+			},
+			expectedErr: &tcpip.ErrNoRoute{},
+		},
+
+		// Connect and SendTo
+		{
+			name: "Connect with NIC then SendTo with different NIC with packet info NIC matching SendTo NIC",
+			connectAddr: tcpip.FullAddress{
+				NIC:  nicID2,
+				Addr: ipv6RemoteAddr2,
+				Port: port,
+			},
+			toAddr: tcpip.FullAddress{
+				NIC:  nicID1,
+				Addr: ipv6RemoteAddr1,
+				Port: port,
+			},
+			pktInfo: tcpip.IPv6PacketInfo{
+				Addr: ipv6Addr1,
+				NIC:  nicID1,
+			},
+			expectedLocalAddr:  ipv6Addr1,
+			expectedRemoteAddr: ipv6RemoteAddr1,
+		},
+	}
+
+	for _, transProto := range []struct {
+		name           string
+		createEndpoint func(*stack.Stack, *waiter.Queue) (tcpip.Endpoint, error)
+	}{
+		{
+			name: "UDP",
+			createEndpoint: func(s *stack.Stack, wq *waiter.Queue) (tcpip.Endpoint, error) {
+				ep, err := s.NewEndpoint(udp.ProtocolNumber, header.IPv6ProtocolNumber, wq)
+				if err != nil {
+					return nil, fmt.Errorf("s.NewEndpoint(%d, %d, _) failed: %s", udp.ProtocolNumber, header.IPv6ProtocolNumber, err)
+				}
+				return ep, nil
+			},
+		},
+		{
+			name: "RAW",
+			createEndpoint: func(s *stack.Stack, wq *waiter.Queue) (tcpip.Endpoint, error) {
+				ep, err := s.NewRawEndpoint(udp.ProtocolNumber, header.IPv6ProtocolNumber, wq, true /* associated */)
+				if err != nil {
+					return nil, fmt.Errorf("s.NewRawEndpoint(%d, %d, _, true) failed: %s", udp.ProtocolNumber, header.IPv6ProtocolNumber, err)
+				}
+				return ep, nil
+			},
+		},
+	} {
+		t.Run(transProto.name, func(t *testing.T) {
+			for _, test := range tests {
+				t.Run(test.name, func(t *testing.T) {
+					s := stack.New(stack.Options{
+						NetworkProtocols:   []stack.NetworkProtocolFactory{ipv6.NewProtocol, ipv4.NewProtocol},
+						TransportProtocols: []stack.TransportProtocolFactory{udp.NewProtocol},
+						RawFactory:         &raw.EndpointFactory{},
+					})
+					e1 := channel.New(1, header.IPv6MinimumMTU, "")
+					if err := s.CreateNIC(nicID1, e1); err != nil {
+						t.Fatalf("s.CreateNIC(%d, _) failed: %s", nicID1, err)
+					}
+					e2 := channel.New(1, header.IPv6MinimumMTU, "")
+					if err := s.CreateNIC(nicID2, e2); err != nil {
+						t.Fatalf("s.CreateNIC(%d, _) failed: %s", nicID2, err)
+					}
+
+					for _, localAddr := range localAddrs {
+						addr := tcpip.ProtocolAddress{
+							Protocol:          header.IPv6ProtocolNumber,
+							AddressWithPrefix: localAddr.addr,
+						}
+						if err := s.AddProtocolAddress(localAddr.nicID, addr, stack.AddressProperties{}); err != nil {
+							t.Fatalf("AddProtocolAddress(%d, %#v, {}): %s", localAddr.nicID, addr, err)
+						}
+					}
+					s.SetRouteTable([]tcpip.Route{
+						// NIC2 before NIC1 to let NIC2 have preference.
+						{
+							Destination: header.IPv6EmptySubnet,
+							NIC:         nicID2,
+						},
+						{
+							Destination: header.IPv6EmptySubnet,
+							NIC:         nicID1,
+						},
+					})
+
+					var wq waiter.Queue
+					ep, err := transProto.createEndpoint(s, &wq)
+					if err != nil {
+						t.Fatalf("transProto.createEndpoint(_) failed: %s", err)
+					}
+					defer ep.Close()
+
+					if err := ep.SocketOptions().SetBindToDevice(int32(test.boundNICID)); err != nil {
+						t.Fatalf("ep.SocketOptions().SetBindToDevice(int32(%d)): %s", test.boundNICID, err)
+					}
+
+					if test.bindAddr != (tcpip.FullAddress{}) {
+						if err := ep.Bind(test.bindAddr); err != nil {
+							t.Fatalf("ep.Bind(%#v): %s", test.bindAddr, err)
+						}
+					}
+
+					if test.connectAddr != (tcpip.FullAddress{}) {
+						if err := ep.Connect(test.connectAddr); err != nil {
+							t.Fatalf("ep.Connect(%#v): %s", test.connectAddr, err)
+						}
+					}
+
+					buf := [...]byte{1, 2, 3, 4}
+					var r bytes.Reader
+					r.Reset(buf[:])
+					opts := tcpip.WriteOptions{
+						ControlMessages: tcpip.SendableControlMessages{
+							HasIPv6PacketInfo: true,
+							IPv6PacketInfo:    test.pktInfo,
+						},
+					}
+					if test.toAddr != (tcpip.FullAddress{}) {
+						opts.To = &test.toAddr
+					}
+
+					if n, err := ep.Write(&r, opts); !cmp.Equal(test.expectedErr, err) {
+						t.Fatalf("got Write(_, %#v) = %s, want = %s", opts, err, test.expectedErr)
+					} else if test.expectedErr != nil {
+						return
+					} else if want := int64(len(buf)); n != want {
+						t.Fatalf("got Write(_, %#v) = %d, want = %d", opts, n, want)
+					}
+
+					{
+						p := e1.Read()
+						if p == nil {
+							t.Fatal("packet didn't arrive at ep1")
+						}
+
+						checker.IPv6(t, stack.PayloadSince(p.NetworkHeader()),
+							checker.SrcAddr(test.expectedLocalAddr),
+							checker.DstAddr(test.expectedRemoteAddr),
+						)
+					}
+
+					if p := e2.Read(); p != nil {
+						t.Errorf("unexpected packet from ep2 = %#v", p)
+					}
 				})
 			}
 		})
