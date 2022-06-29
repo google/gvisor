0acccf664a013cdabc97e1468666b14ec5b314f7
Kevin Krakauer <krakauer@google.com>
2022-06-29 10:48:42 -0700

    fix copyright date
    PiperOrigin-RevId: 458012755

--- test/e2e/integration_runtime_test.go
+++ test/e2e/integration_runtime_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The gVisor Authors.
+// Copyright 2022 The gVisor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
