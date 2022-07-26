d0244d1efbd55ec51699454fe1c74794113f34a3
Zach Koopmans <zkoopmans@google.com>
2022-07-26 11:56:30 -0700

    [benchmarks] Remove broken runsc benchmark.
    This has been replaced by the gRPC build benchmark
    which is below the removed BuildRunsc benchmark.
    
    PiperOrigin-RevId: 463393704

--- test/benchmarks/fs/bazel_test.go
+++ test/benchmarks/fs/bazel_test.go
@@ -30,12 +30,6 @@ func BenchmarkBuildABSL(b *testing.B) {
 }
 
 // Note: CleanCache versions of this test require running with root permissions.
-// Note: This test takes on the order of 10m per permutation for runsc on kvm.
-func BenchmarkBuildRunsc(b *testing.B) {
-	runBuildBenchmark(b, "benchmarks/runsc", "/gvisor", "runsc:runsc")
-}
-
-// Note: CleanCache versions of this test require running with root permissions.
 // Note: This test takes on the order of 6m per permutation for runsc on kvm.
 func BenchmarkBuildGRPC(b *testing.B) {
 	runBuildBenchmark(b, "benchmarks/build-grpc", "/grpc", ":grpc")
