09dbb4e1c245c3576abd59eb365bf8ace54690cd
Chen Hui <cedriccchen@tencent.com>
2022-09-19 16:09:25 +0800

    benchmarks: add BenchmarkIperfParameterized testcase for iperf benchmark
    Signed-off-by: Chen Hui <cedriccchen@tencent.com>

--- test/benchmarks/network/iperf_test.go
+++ test/benchmarks/network/iperf_test.go
@@ -106,6 +106,170 @@ func BenchmarkIperf(b *testing.B) {
 	}
 }
 
+func BenchmarkIperfParameterized(b *testing.B) {
+	clientMachine, err := harness.GetMachine()
+	if err != nil {
+		b.Fatalf("failed to get machine: %v", err)
+	}
+	defer clientMachine.CleanUp()
+
+	serverMachine, err := harness.GetMachine()
+	if err != nil {
+		b.Fatalf("failed to get machine: %v", err)
+	}
+	defer serverMachine.CleanUp()
+	ctx := context.Background()
+	for _, bm := range []struct {
+		name       string
+		length     int
+		parallel   int
+		clientFunc func(context.Context, testutil.Logger) *dockerutil.Container
+		serverFunc func(context.Context, testutil.Logger) *dockerutil.Container
+	}{
+		// We are either measuring the server or the client. The other should be
+		// runc. e.g. Upload sees how fast the runtime under test uploads to a native
+		// server.
+		{
+			name:       "Upload",
+			length:     4,
+			parallel:   1,
+			clientFunc: clientMachine.GetContainer,
+			serverFunc: serverMachine.GetNativeContainer,
+		},
+		{
+			name:       "Upload",
+			length:     64,
+			parallel:   1,
+			clientFunc: clientMachine.GetContainer,
+			serverFunc: serverMachine.GetNativeContainer,
+		},
+		{
+			name:       "Upload",
+			length:     1024,
+			parallel:   1,
+			clientFunc: clientMachine.GetContainer,
+			serverFunc: serverMachine.GetNativeContainer,
+		},
+		{
+			name:       "Upload",
+			length:     4,
+			parallel:   16,
+			clientFunc: clientMachine.GetContainer,
+			serverFunc: serverMachine.GetNativeContainer,
+		},
+		{
+			name:       "Upload",
+			length:     64,
+			parallel:   16,
+			clientFunc: clientMachine.GetContainer,
+			serverFunc: serverMachine.GetNativeContainer,
+		},
+		{
+			name:       "Upload",
+			length:     1024,
+			parallel:   16,
+			clientFunc: clientMachine.GetContainer,
+			serverFunc: serverMachine.GetNativeContainer,
+		},
+		{
+			name:       "Download",
+			length:     4,
+			parallel:   1,
+			clientFunc: clientMachine.GetNativeContainer,
+			serverFunc: serverMachine.GetContainer,
+		},
+		{
+			name:       "Download",
+			length:     64,
+			parallel:   1,
+			clientFunc: clientMachine.GetNativeContainer,
+			serverFunc: serverMachine.GetContainer,
+		},
+		{
+			name:       "Download",
+			length:     1024,
+			parallel:   1,
+			clientFunc: clientMachine.GetNativeContainer,
+			serverFunc: serverMachine.GetContainer,
+		},
+		{
+			name:       "Download",
+			length:     4,
+			parallel:   16,
+			clientFunc: clientMachine.GetNativeContainer,
+			serverFunc: serverMachine.GetContainer,
+		},
+		{
+			name:       "Download",
+			length:     64,
+			parallel:   16,
+			clientFunc: clientMachine.GetNativeContainer,
+			serverFunc: serverMachine.GetContainer,
+		},
+		{
+			name:       "Download",
+			length:     1024,
+			parallel:   16,
+			clientFunc: clientMachine.GetNativeContainer,
+			serverFunc: serverMachine.GetContainer,
+		},
+	} {
+		name, err := tools.ParametersToName(tools.Parameter{
+			Name:  "operation",
+			Value: bm.name,
+		}, tools.Parameter{
+			Name:  "length",
+			Value: fmt.Sprintf("%dK", bm.length),
+		}, tools.Parameter{
+			Name:  "parallel",
+			Value: fmt.Sprintf("%d", bm.parallel),
+		})
+		if err != nil {
+			b.Fatalf("Failed to parse parameters: %v", err)
+		}
+		b.Run(name, func(b *testing.B) {
+			// Set up the containers.
+			server := bm.serverFunc(ctx, b)
+			defer server.CleanUp(ctx)
+			client := bm.clientFunc(ctx, b)
+			defer client.CleanUp(ctx)
+
+			// iperf serves on port 5001 by default.
+			port := 5001
+
+			// Start the server.
+			if err := server.Spawn(ctx, dockerutil.RunOpts{
+				Image: "benchmarks/iperf",
+				Ports: []int{port},
+			}, "iperf", "-s"); err != nil {
+				b.Fatalf("failed to start server with: %v", err)
+			}
+			if out, err := server.WaitForOutput(ctx, fmt.Sprintf("Server listening on TCP port %d", port), 10*time.Second); err != nil {
+				b.Fatalf("failed to wait for iperf server: %v %s", err, out)
+			}
+
+			iperf := tools.Iperf{
+				Num:      b.N,       // KB for the client to send.
+				Length:   bm.length, // KB for length.
+				Parallel: bm.parallel,
+			}
+
+			// Run the client.
+			b.ResetTimer()
+			out, err := client.Run(ctx, dockerutil.RunOpts{
+				Image: "benchmarks/iperf",
+				Links: []string{server.MakeLink("iperfsrv")},
+			}, iperf.MakeCmd("iperfsrv", port)...)
+			if err != nil {
+				b.Fatalf("failed to run client: %v", err)
+			}
+			b.StopTimer()
+			iperf.Report(b, out)
+			b.StartTimer()
+		})
+	}
+}
+
 func TestMain(m *testing.M) {
 	harness.Init()
 	os.Exit(m.Run())
