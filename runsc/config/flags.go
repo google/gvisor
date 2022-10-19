2115c4d444f16dbb5a5c87a64f4ba86e41cec355
Lucas Manning <lucasmanning@google.com>
2022-10-19 09:45:20 -0700

    Make buffer pooling true by default in runsc.
    PiperOrigin-RevId: 482229293

--- runsc/config/flags.go
+++ runsc/config/flags.go
@@ -97,7 +97,7 @@ func RegisterFlags(flagSet *flag.FlagSet) {
 	flagSet.Bool("rx-checksum-offload", true, "enable RX checksum offload.")
 	flagSet.Var(queueingDisciplinePtr(QDiscFIFO), "qdisc", "specifies which queueing discipline to apply by default to the non loopback nics used by the sandbox.")
 	flagSet.Int("num-network-channels", 1, "number of underlying channels(FDs) to use for network link endpoints.")
-	flagSet.Bool("buffer-pooling", false, "enable allocation of buffers from a shared pool instead of the heap.")
+	flagSet.Bool("buffer-pooling", true, "enable allocation of buffers from a shared pool instead of the heap.")
 	flagSet.Bool("EXPERIMENTAL-afxdp", false, "EXPERIMENTAL. Use an AF_XDP socket to receive packets.")
 
 	// Test flags, not to be used outside tests, ever.
