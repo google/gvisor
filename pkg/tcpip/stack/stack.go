cfb0f3e5226349b857c562c11c6049f7be0e64e2
Nicolas Lacasse <nlacasse@google.com>
2022-12-13 15:23:01 -0800

    Lock nic.networkEndpoints with nic.mu.
    PiperOrigin-RevId: 495141130

--- pkg/tcpip/stack/stack.go
+++ pkg/tcpip/stack/stack.go
@@ -1049,9 +1049,11 @@ func (s *Stack) NICInfo() map[tcpip.NICID]NICInfo {
 		}
 
 		netStats := make(map[tcpip.NetworkProtocolNumber]NetworkEndpointStats)
+		nic.mu.RLock()
 		for proto, netEP := range nic.networkEndpoints {
 			netStats[proto] = netEP.Stats()
 		}
+		nic.mu.RUnlock()
 
 		info := NICInfo{
 			Name:                nic.name,
