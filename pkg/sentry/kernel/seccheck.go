13b3dac5946330396e98e33b044257a002ef1d64
Lucas Manning <lucasmanning@google.com>
2023-01-09 10:22:37 -0800

    Fix circular lock between filesystemRWMutex and taskSetRWMutex.
    PiperOrigin-RevId: 500747195

--- pkg/sentry/kernel/seccheck.go
+++ pkg/sentry/kernel/seccheck.go
@@ -19,17 +19,44 @@ import (
 	pb "gvisor.dev/gvisor/pkg/sentry/seccheck/points/points_go_proto"
 )
 
+func getTaskCurrentWorkingDirectory(t *Task) string {
+	// Grab the filesystem context first since it needs tasks.mu to be locked.
+	// It's safe to unlock and use the values obtained here as long as there's
+	// no way to modify root and wd from a separate task.
+	t.k.tasks.mu.RLock()
+	root := t.FSContext().RootDirectory()
+	wd := t.FSContext().WorkingDirectory()
+	t.k.tasks.mu.RUnlock()
+
+	// Perform VFS operations outside of task mutex to avoid circular locking with
+	// filesystem mutexes.
+	var cwd string
+	if root.Ok() {
+		defer root.DecRef(t)
+		if wd.Ok() {
+			defer wd.DecRef(t)
+			vfsObj := root.Mount().Filesystem().VirtualFilesystem()
+			cwd, _ = vfsObj.PathnameWithDeleted(t, root, wd)
+		}
+	}
+	return cwd
+}
+
 // LoadSeccheckData sets info from the task based on mask.
 func LoadSeccheckData(t *Task, mask seccheck.FieldMask, info *pb.ContextData) {
+	var cwd string
+	if mask.Contains(seccheck.FieldCtxtCwd) {
+		cwd = getTaskCurrentWorkingDirectory(t)
+	}
 	t.k.tasks.mu.RLock()
 	defer t.k.tasks.mu.RUnlock()
-	LoadSeccheckDataLocked(t, mask, info)
+	LoadSeccheckDataLocked(t, mask, info, cwd)
 }
 
 // LoadSeccheckDataLocked sets info from the task based on mask.
 //
 // Preconditions: The TaskSet mutex must be locked.
-func LoadSeccheckDataLocked(t *Task, mask seccheck.FieldMask, info *pb.ContextData) {
+func LoadSeccheckDataLocked(t *Task, mask seccheck.FieldMask, info *pb.ContextData, cwd string) {
 	if mask.Contains(seccheck.FieldCtxtTime) {
 		info.TimeNs = t.k.RealtimeClock().Now().Nanoseconds()
 	}
@@ -49,14 +76,7 @@ func LoadSeccheckDataLocked(t *Task, mask seccheck.FieldMask, info *pb.ContextDa
 		info.ContainerId = t.tg.leader.ContainerID()
 	}
 	if mask.Contains(seccheck.FieldCtxtCwd) {
-		if root := t.FSContext().RootDirectory(); root.Ok() {
-			defer root.DecRef(t)
-			if wd := t.FSContext().WorkingDirectory(); wd.Ok() {
-				defer wd.DecRef(t)
-				vfsObj := root.Mount().Filesystem().VirtualFilesystem()
-				info.Cwd, _ = vfsObj.PathnameWithDeleted(t, root, wd)
-			}
-		}
+		info.Cwd = cwd
 	}
 	if mask.Contains(seccheck.FieldCtxtProcessName) {
 		info.ProcessName = t.Name()
