375b4495ded43870968551e88f585a69a6f65569
Michael Pratt <mpratt@google.com>
2023-01-27 11:54:04 -0800

    Split remaining gohacks functions into separate files
    Functions using linkname are checked via checklinkname. Noescape has no
    automatic check anymore, but if a future version of Go invalidates the
    implementation, the result will be values escaping to the heap (performance
    loss), rather than memory corruption, so I think having no check is OK.
    
    PiperOrigin-RevId: 505173611

--- pkg/gohacks/linkname_go113_unsafe.go
+++ pkg/gohacks/linkname_go113_unsafe.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The gVisor Authors.
+// Copyright 2023 The gVisor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.13 && !go1.21
-// +build go1.13,!go1.21
+//go:build go1.13
 
 // //go:linkname directives type-checked by checklinkname. Any other
 // non-linkname assumptions outside the Go 1 compatibility guarantee should
 // have an accompanied vet check or version guard build tag.
-
-// Check type signatures and Noescape when updating Go version.
-//
-// TODO(b/165820485): add these checks to checklinkname.
 
 // Package gohacks contains utilities for subverting the Go compiler.
 package gohacks
@@ -29,19 +24,6 @@ package gohacks
 import (
 	"unsafe"
 )
-
-// Noescape hides a pointer from escape analysis. Noescape is the identity
-// function but escape analysis doesn't think the output depends on the input.
-// Noescape is inlined and currently compiles down to zero instructions.
-// USE CAREFULLY!
-//
-// (Noescape is copy/pasted from Go's runtime/stubs.go:noescape().)
-//
-//go:nosplit
-func Noescape(p unsafe.Pointer) unsafe.Pointer {
-	x := uintptr(p)
-	return unsafe.Pointer(x ^ 0)
-}
 
 // Note that go:linkname silently doesn't work if the local name is exported,
 // necessitating an indirection for exported functions.
