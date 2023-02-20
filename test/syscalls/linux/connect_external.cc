6aaaa0ec8fd170fe33254a73b6055d992f61a420
Ayush Ranjan <ayushranjan@google.com>
2023-02-19 16:35:22 -0800

    Deflake test/syscalls:connect_external_test_native.
    This native test was flaking on Buildkite because it would timeout. The test
    itself would pass but I think a cleanup stage would hang indefinitely. Such a
    hang would only happen when one of the cleaners from uds.go:createPipeReader()
    would fail with the following log:
    `Failed to kick pipe reader: open /tmp/sockets930502909/pipe/out: no such
    device or address`.
    
    The connect_external_test does not even use pipes. It is unnecessary setup and
    cleanup steps. Furthermore, test logs are very noisy from these such setup and
    cleanup steps for tests that are not even run.
    
    Although I am not sure why failing to kick the pipe reader would cause the test
    to hang, I have split out the setup done in `CreateSocketTree()` into 3 parts:
    1. Creating a UDS tree so that sandbox can connect to it.
    2. Create connectors outside sandbox who connect to sockets created by sandbox.
    3. Creating pipes outside the sandbox that sandbox can read/write to.
    
    I also split the test targets into separate tests. These setup steps are only
    done for their respective tests. Makes the logs more relevant, gets rid of
    unnecessary steps and failures and deflakes the native connect test.
    
    PiperOrigin-RevId: 510862461

--- test/syscalls/linux/connect_external.cc
+++ test/syscalls/linux/connect_external.cc
@@ -12,17 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <asm-generic/errno-base.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <sys/un.h>
 
 #include <cstring>
 #include <string>
 #include <tuple>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "test/util/file_descriptor.h"
 #include "test/util/fs_util.h"
@@ -110,60 +109,6 @@ TEST_P(GoferStreamSeqpacketTest, NonListening) {
   ASSERT_THAT(connect(sock.get(), reinterpret_cast<struct sockaddr*>(&addr),
                       sizeof(addr)),
               SyscallFailsWithErrno(ECONNREFUSED));
-}
-
-// Bind to a socket, then Listen and Accept.
-TEST_P(GoferStreamSeqpacketTest, BindListenAccept) {
-  std::string env;
-  ProtocolSocket proto;
-  std::tie(env, proto) = GetParam();
-
-  // Do not parametrize this test with attach tree variant. This test creates a
-  // new UDS via bind(2). It is not possible to bind mount a non-existing file.
-  SKIP_IF(!strcmp("TEST_UDS_ATTACH_TREE", env.c_str()));
-
-  char* val = getenv(env.c_str());
-  ASSERT_NE(val, nullptr);
-  std::string root(val);
-
-  FileDescriptor sock =
-      ASSERT_NO_ERRNO_AND_VALUE(Socket(AF_UNIX, proto.protocol, 0));
-
-  std::string socket_path = JoinPath(root, proto.name, "created-in-sandbox");
-
-  struct sockaddr_un addr = {};
-  addr.sun_family = AF_UNIX;
-  memcpy(addr.sun_path, socket_path.c_str(), socket_path.length());
-
-  ASSERT_THAT(
-      bind(sock.get(), reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr)),
-      SyscallSucceeds());
-  ASSERT_THAT(listen(sock.get(), 1), SyscallSucceeds());
-
-  // Bind again on that socket with a diff address should fail.
-  std::string socket_path2 = socket_path + "-fail";
-  struct sockaddr_un addr2 = {};
-  addr2.sun_family = AF_UNIX;
-  memcpy(addr2.sun_path, socket_path2.c_str(), socket_path2.length());
-  ASSERT_THAT(bind(sock.get(), reinterpret_cast<struct sockaddr*>(&addr2),
-                   sizeof(addr2)),
-              SyscallFailsWithErrno(EINVAL));
-
-  FileDescriptor accSock =
-      ASSERT_NO_ERRNO_AND_VALUE(Accept(sock.get(), NULL, NULL));
-
-  // Other socket should be echo server.
-  constexpr int kBufferSize = 64;
-  char send_buffer[kBufferSize];
-  memset(send_buffer, 'a', sizeof(send_buffer));
-
-  ASSERT_THAT(WriteFd(accSock.get(), send_buffer, sizeof(send_buffer)),
-              SyscallSucceedsWithValue(sizeof(send_buffer)));
-
-  char recv_buffer[kBufferSize];
-  ASSERT_THAT(ReadFd(accSock.get(), recv_buffer, sizeof(recv_buffer)),
-              SyscallSucceedsWithValue(sizeof(recv_buffer)));
-  ASSERT_EQ(0, memcmp(send_buffer, recv_buffer, sizeof(send_buffer)));
 }
 
 INSTANTIATE_TEST_SUITE_P(
