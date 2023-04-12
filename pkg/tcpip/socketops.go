d27ebcd8bd2f76b4dc26f9696233e78411be3177
Tony Gong <gongt@google.com>
2023-04-12 10:42:48 -0700

    Add socket-level socket option stats
    Add stats to the SocketOptions struct which tracks the number of times
    each socket option is get and set.
    
    PiperOrigin-RevId: 523743002

--- pkg/tcpip/socketops.go
+++ pkg/tcpip/socketops.go
@@ -123,6 +123,65 @@ type StackHandler interface {
 	TransportProtocolOption(proto TransportProtocolNumber, option GettableTransportProtocolOption) Error
 }
 
+// SocketOptionStats tracks the number of times each socket-level socket option
+// is get and set.
+type SocketOptionStats struct {
+	GetBroadcastEnabled             StatCounter
+	SetBroadcastEnabled             StatCounter
+	GetPassCredEnabled              StatCounter
+	SetPassCredEnabled              StatCounter
+	GetNoChecksumEnabled            StatCounter
+	SetNoChecksumEnabled            StatCounter
+	GetReuseAddressEnabled          StatCounter
+	SetReuseAddressEnabled          StatCounter
+	GetReusePortEnabled             StatCounter
+	SetReusePortEnabled             StatCounter
+	GetKeepAliveEnabled             StatCounter
+	SetKeepAliveEnabled             StatCounter
+	GetMulticastLoopEnabled         StatCounter
+	SetMulticastLoopEnabled         StatCounter
+	GetReceiveTOSEnabled            StatCounter
+	SetReceiveTOSEnabled            StatCounter
+	GetReceiveTTLEnabled            StatCounter
+	SetReceiveTTLEnabled            StatCounter
+	GetReceiveHopLimitEnabled       StatCounter
+	SetReceiveHopLimitEnabled       StatCounter
+	GetReceiveTClassEnabled         StatCounter
+	SetReceiveTClassEnabled         StatCounter
+	GetReceivePacketInfoEnabled     StatCounter
+	SetReceivePacketInfoEnabled     StatCounter
+	GetReceiveIPv6PacketInfoEnabled StatCounter
+	SetReceiveIPv6PacketInfoEnabled StatCounter
+	GetHdrIncludedEnabled           StatCounter
+	SetHdrIncludedEnabled           StatCounter
+	GetV6OnlyEnabled                StatCounter
+	SetV6OnlyEnabled                StatCounter
+	GetQuickAckEnabled              StatCounter
+	SetQuickAckEnabled              StatCounter
+	GetDelayOptionEnabled           StatCounter
+	SetDelayOptionEnabled           StatCounter
+	GetCorkOptionEnabled            StatCounter
+	SetCorkOptionEnabled            StatCounter
+	GetReceiveOriginalDstAddress    StatCounter
+	SetReceiveOriginalDstAddress    StatCounter
+	GetIpv4RecvErrEnabled           StatCounter
+	SetIpv4RecvErrEnabled           StatCounter
+	GetIpv6RecvErrEnabled           StatCounter
+	SetIpv6RecvErrEnabled           StatCounter
+	GetBindToDevice                 StatCounter
+	SetBindToDevice                 StatCounter
+	GetSendBufferSize               StatCounter
+	SetSendBufferSize               StatCounter
+	GetReceiveBufferSize            StatCounter
+	SetReceiveBufferSize            StatCounter
+	GetLinger                       StatCounter
+	SetLinger                       StatCounter
+	GetRcvlowat                     StatCounter
+	SetRcvlowat                     StatCounter
+	GetOutOfBandInline              StatCounter
+	SetOutOfBandInline              StatCounter
+}
+
 // SocketOptions contains all the variables which store values for SOL_SOCKET,
 // SOL_IP, SOL_IPV6 and SOL_TCP level options.
 //
@@ -132,6 +191,9 @@ type SocketOptions struct {
 
 	// StackHandler is initialized at the creation time and will not change.
 	stackHandler StackHandler `state:"manual"`
+
+	// stats tracks the number of times a socket option is get/set.
+	stats SocketOptionStats
 
 	// These fields are accessed and modified using atomic operations.
 
@@ -265,6 +327,11 @@ func (so *SocketOptions) InitHandler(handler SocketOptionsHandler, stack StackHa
 	so.getReceiveBufferLimits = getReceiveBufferLimits
 }
 
+// Stats returns socket option stats.
+func (so *SocketOptions) Stats() *SocketOptionStats {
+	return &so.stats
+}
+
 func storeAtomicBool(addr *atomicbitops.Uint32, v bool) {
 	var val uint32
 	if v {
@@ -280,149 +347,178 @@ func (so *SocketOptions) SetLastError(err Error) {
 
 // GetBroadcast gets value for SO_BROADCAST option.
 func (so *SocketOptions) GetBroadcast() bool {
+	so.stats.GetBroadcastEnabled.Increment()
 	return so.broadcastEnabled.Load() != 0
 }
 
 // SetBroadcast sets value for SO_BROADCAST option.
 func (so *SocketOptions) SetBroadcast(v bool) {
+	so.stats.SetBroadcastEnabled.Increment()
 	storeAtomicBool(&so.broadcastEnabled, v)
 }
 
 // GetPassCred gets value for SO_PASSCRED option.
 func (so *SocketOptions) GetPassCred() bool {
+	so.stats.GetPassCredEnabled.Increment()
 	return so.passCredEnabled.Load() != 0
 }
 
 // SetPassCred sets value for SO_PASSCRED option.
 func (so *SocketOptions) SetPassCred(v bool) {
+	so.stats.SetPassCredEnabled.Increment()
 	storeAtomicBool(&so.passCredEnabled, v)
 }
 
 // GetNoChecksum gets value for SO_NO_CHECK option.
 func (so *SocketOptions) GetNoChecksum() bool {
+	so.stats.GetNoChecksumEnabled.Increment()
 	return so.noChecksumEnabled.Load() != 0
 }
 
 // SetNoChecksum sets value for SO_NO_CHECK option.
 func (so *SocketOptions) SetNoChecksum(v bool) {
+	so.stats.SetNoChecksumEnabled.Increment()
 	storeAtomicBool(&so.noChecksumEnabled, v)
 }
 
 // GetReuseAddress gets value for SO_REUSEADDR option.
 func (so *SocketOptions) GetReuseAddress() bool {
+	so.stats.GetReuseAddressEnabled.Increment()
 	return so.reuseAddressEnabled.Load() != 0
 }
 
 // SetReuseAddress sets value for SO_REUSEADDR option.
 func (so *SocketOptions) SetReuseAddress(v bool) {
+	so.stats.SetReuseAddressEnabled.Increment()
 	storeAtomicBool(&so.reuseAddressEnabled, v)
 	so.handler.OnReuseAddressSet(v)
 }
 
 // GetReusePort gets value for SO_REUSEPORT option.
 func (so *SocketOptions) GetReusePort() bool {
+	so.stats.GetReusePortEnabled.Increment()
 	return so.reusePortEnabled.Load() != 0
 }
 
 // SetReusePort sets value for SO_REUSEPORT option.
 func (so *SocketOptions) SetReusePort(v bool) {
+	so.stats.SetReusePortEnabled.Increment()
 	storeAtomicBool(&so.reusePortEnabled, v)
 	so.handler.OnReusePortSet(v)
 }
 
 // GetKeepAlive gets value for SO_KEEPALIVE option.
 func (so *SocketOptions) GetKeepAlive() bool {
+	so.stats.GetKeepAliveEnabled.Increment()
 	return so.keepAliveEnabled.Load() != 0
 }
 
 // SetKeepAlive sets value for SO_KEEPALIVE option.
 func (so *SocketOptions) SetKeepAlive(v bool) {
+	so.stats.SetKeepAliveEnabled.Increment()
 	storeAtomicBool(&so.keepAliveEnabled, v)
 	so.handler.OnKeepAliveSet(v)
 }
 
 // GetMulticastLoop gets value for IP_MULTICAST_LOOP option.
 func (so *SocketOptions) GetMulticastLoop() bool {
+	so.stats.GetMulticastLoopEnabled.Increment()
 	return so.multicastLoopEnabled.Load() != 0
 }
 
 // SetMulticastLoop sets value for IP_MULTICAST_LOOP option.
 func (so *SocketOptions) SetMulticastLoop(v bool) {
+	so.stats.SetMulticastLoopEnabled.Increment()
 	storeAtomicBool(&so.multicastLoopEnabled, v)
 }
 
 // GetReceiveTOS gets value for IP_RECVTOS option.
 func (so *SocketOptions) GetReceiveTOS() bool {
+	so.stats.GetReceiveTOSEnabled.Increment()
 	return so.receiveTOSEnabled.Load() != 0
 }
 
 // SetReceiveTOS sets value for IP_RECVTOS option.
 func (so *SocketOptions) SetReceiveTOS(v bool) {
+	so.stats.SetReceiveTOSEnabled.Increment()
 	storeAtomicBool(&so.receiveTOSEnabled, v)
 }
 
 // GetReceiveTTL gets value for IP_RECVTTL option.
 func (so *SocketOptions) GetReceiveTTL() bool {
+	so.stats.GetReceiveTTLEnabled.Increment()
 	return so.receiveTTLEnabled.Load() != 0
 }
 
 // SetReceiveTTL sets value for IP_RECVTTL option.
 func (so *SocketOptions) SetReceiveTTL(v bool) {
+	so.stats.SetReceiveTTLEnabled.Increment()
 	storeAtomicBool(&so.receiveTTLEnabled, v)
 }
 
 // GetReceiveHopLimit gets value for IP_RECVHOPLIMIT option.
 func (so *SocketOptions) GetReceiveHopLimit() bool {
+	so.stats.GetReceiveHopLimitEnabled.Increment()
 	return so.receiveHopLimitEnabled.Load() != 0
 }
 
 // SetReceiveHopLimit sets value for IP_RECVHOPLIMIT option.
 func (so *SocketOptions) SetReceiveHopLimit(v bool) {
+	so.stats.SetReceiveHopLimitEnabled.Increment()
 	storeAtomicBool(&so.receiveHopLimitEnabled, v)
 }
 
 // GetReceiveTClass gets value for IPV6_RECVTCLASS option.
 func (so *SocketOptions) GetReceiveTClass() bool {
+	so.stats.GetReceiveTClassEnabled.Increment()
 	return so.receiveTClassEnabled.Load() != 0
 }
 
 // SetReceiveTClass sets value for IPV6_RECVTCLASS option.
 func (so *SocketOptions) SetReceiveTClass(v bool) {
+	so.stats.SetReceiveTClassEnabled.Increment()
 	storeAtomicBool(&so.receiveTClassEnabled, v)
 }
 
 // GetReceivePacketInfo gets value for IP_PKTINFO option.
 func (so *SocketOptions) GetReceivePacketInfo() bool {
+	so.stats.GetReceivePacketInfoEnabled.Increment()
 	return so.receivePacketInfoEnabled.Load() != 0
 }
 
 // SetReceivePacketInfo sets value for IP_PKTINFO option.
 func (so *SocketOptions) SetReceivePacketInfo(v bool) {
+	so.stats.SetReceivePacketInfoEnabled.Increment()
 	storeAtomicBool(&so.receivePacketInfoEnabled, v)
 }
 
 // GetIPv6ReceivePacketInfo gets value for IPV6_RECVPKTINFO option.
 func (so *SocketOptions) GetIPv6ReceivePacketInfo() bool {
+	so.stats.GetReceiveIPv6PacketInfoEnabled.Increment()
 	return so.receiveIPv6PacketInfoEnabled.Load() != 0
 }
 
 // SetIPv6ReceivePacketInfo sets value for IPV6_RECVPKTINFO option.
 func (so *SocketOptions) SetIPv6ReceivePacketInfo(v bool) {
+	so.stats.SetReceiveIPv6PacketInfoEnabled.Increment()
 	storeAtomicBool(&so.receiveIPv6PacketInfoEnabled, v)
 }
 
 // GetHeaderIncluded gets value for IP_HDRINCL option.
 func (so *SocketOptions) GetHeaderIncluded() bool {
+	so.stats.GetHdrIncludedEnabled.Increment()
 	return so.hdrIncludedEnabled.Load() != 0
 }
 
 // SetHeaderIncluded sets value for IP_HDRINCL option.
 func (so *SocketOptions) SetHeaderIncluded(v bool) {
+	so.stats.SetHdrIncludedEnabled.Increment()
 	storeAtomicBool(&so.hdrIncludedEnabled, v)
 }
 
 // GetV6Only gets value for IPV6_V6ONLY option.
 func (so *SocketOptions) GetV6Only() bool {
+	so.stats.GetV6OnlyEnabled.Increment()
 	return so.v6OnlyEnabled.Load() != 0
 }
 
@@ -430,58 +526,69 @@ func (so *SocketOptions) GetV6Only() bool {
 //
 // Preconditions: the backing TCP or UDP endpoint must be in initial state.
 func (so *SocketOptions) SetV6Only(v bool) {
+	so.stats.SetV6OnlyEnabled.Increment()
 	storeAtomicBool(&so.v6OnlyEnabled, v)
 }
 
 // GetQuickAck gets value for TCP_QUICKACK option.
 func (so *SocketOptions) GetQuickAck() bool {
+	so.stats.GetQuickAckEnabled.Increment()
 	return so.quickAckEnabled.Load() != 0
 }
 
 // SetQuickAck sets value for TCP_QUICKACK option.
 func (so *SocketOptions) SetQuickAck(v bool) {
+	so.stats.SetQuickAckEnabled.Increment()
 	storeAtomicBool(&so.quickAckEnabled, v)
 }
 
 // GetDelayOption gets inverted value for TCP_NODELAY option.
 func (so *SocketOptions) GetDelayOption() bool {
+	so.stats.GetDelayOptionEnabled.Increment()
 	return so.delayOptionEnabled.Load() != 0
 }
 
 // SetDelayOption sets inverted value for TCP_NODELAY option.
 func (so *SocketOptions) SetDelayOption(v bool) {
+	so.stats.SetDelayOptionEnabled.Increment()
 	storeAtomicBool(&so.delayOptionEnabled, v)
 	so.handler.OnDelayOptionSet(v)
 }
 
 // GetCorkOption gets value for TCP_CORK option.
 func (so *SocketOptions) GetCorkOption() bool {
+	so.stats.GetCorkOptionEnabled.Increment()
 	return so.corkOptionEnabled.Load() != 0
 }
 
 // SetCorkOption sets value for TCP_CORK option.
 func (so *SocketOptions) SetCorkOption(v bool) {
+	so.stats.SetCorkOptionEnabled.Increment()
 	storeAtomicBool(&so.corkOptionEnabled, v)
 	so.handler.OnCorkOptionSet(v)
 }
 
 // GetReceiveOriginalDstAddress gets value for IP(V6)_RECVORIGDSTADDR option.
 func (so *SocketOptions) GetReceiveOriginalDstAddress() bool {
+	so.stats.GetReceiveOriginalDstAddress.Increment()
 	return so.receiveOriginalDstAddress.Load() != 0
 }
 
 // SetReceiveOriginalDstAddress sets value for IP(V6)_RECVORIGDSTADDR option.
 func (so *SocketOptions) SetReceiveOriginalDstAddress(v bool) {
+	so.stats.SetReceiveOriginalDstAddress.Increment()
 	storeAtomicBool(&so.receiveOriginalDstAddress, v)
 }
 
 // GetIPv4RecvError gets value for IP_RECVERR option.
 func (so *SocketOptions) GetIPv4RecvError() bool {
+	so.stats.GetIpv4RecvErrEnabled.Increment()
 	return so.ipv4RecvErrEnabled.Load() != 0
 }
 
 // SetIPv4RecvError sets value for IP_RECVERR option.
 func (so *SocketOptions) SetIPv4RecvError(v bool) {
+	so.stats.SetIpv4RecvErrEnabled.Increment()
 	storeAtomicBool(&so.ipv4RecvErrEnabled, v)
 	if !v {
 		so.pruneErrQueue()
@@ -490,11 +597,13 @@ func (so *SocketOptions) SetIPv4RecvError(v bool) {
 
 // GetIPv6RecvError gets value for IPV6_RECVERR option.
 func (so *SocketOptions) GetIPv6RecvError() bool {
+	so.stats.GetIpv6RecvErrEnabled.Increment()
 	return so.ipv6RecvErrEnabled.Load() != 0
 }
 
 // SetIPv6RecvError sets value for IPV6_RECVERR option.
 func (so *SocketOptions) SetIPv6RecvError(v bool) {
+	so.stats.SetIpv6RecvErrEnabled.Increment()
 	storeAtomicBool(&so.ipv6RecvErrEnabled, v)
 	if !v {
 		so.pruneErrQueue()
@@ -507,16 +616,20 @@ func (so *SocketOptions) GetLastError() Error {
 }
 
 // GetOutOfBandInline gets value for SO_OOBINLINE option.
-func (*SocketOptions) GetOutOfBandInline() bool {
+func (so *SocketOptions) GetOutOfBandInline() bool {
+	so.stats.GetOutOfBandInline.Increment()
 	return true
 }
 
 // SetOutOfBandInline sets value for SO_OOBINLINE option. We currently do not
 // support disabling this option.
-func (*SocketOptions) SetOutOfBandInline(bool) {}
+func (so *SocketOptions) SetOutOfBandInline(bool) {
+	so.stats.SetOutOfBandInline.Increment()
+}
 
 // GetLinger gets value for SO_LINGER option.
 func (so *SocketOptions) GetLinger() LingerOption {
+	so.stats.GetLinger.Increment()
 	so.mu.Lock()
 	linger := so.linger
 	so.mu.Unlock()
@@ -525,6 +638,7 @@ func (so *SocketOptions) GetLinger() LingerOption {
 
 // SetLinger sets value for SO_LINGER option.
 func (so *SocketOptions) SetLinger(linger LingerOption) {
+	so.stats.SetLinger.Increment()
 	so.mu.Lock()
 	so.linger = linger
 	so.mu.Unlock()
@@ -665,6 +779,7 @@ func (so *SocketOptions) QueueLocalErr(err Error, net NetworkProtocolNumber, inf
 
 // GetBindToDevice gets value for SO_BINDTODEVICE option.
 func (so *SocketOptions) GetBindToDevice() int32 {
+	so.stats.GetBindToDevice.Increment()
 	return so.bindToDevice.Load()
 }
 
@@ -681,6 +796,7 @@ func (so *SocketOptions) SetBindToDevice(bindToDevice int32) Error {
 
 // GetSendBufferSize gets value for SO_SNDBUF option.
 func (so *SocketOptions) GetSendBufferSize() int64 {
+	so.stats.GetSendBufferSize.Increment()
 	return so.sendBufferSize.Load()
 }
 
@@ -694,6 +810,7 @@ func (so *SocketOptions) SendBufferLimits() (min, max int64) {
 // SetSendBufferSize sets value for SO_SNDBUF option. notify indicates if the
 // stack handler should be invoked to set the send buffer size.
 func (so *SocketOptions) SetSendBufferSize(sendBufferSize int64, notify bool) {
+	so.stats.SetSendBufferSize.Increment()
 	if notify {
 		sendBufferSize = so.handler.OnSetSendBufferSize(sendBufferSize)
 	}
@@ -705,6 +822,7 @@ func (so *SocketOptions) SetSendBufferSize(sendBufferSize int64, notify bool) {
 
 // GetReceiveBufferSize gets value for SO_RCVBUF option.
 func (so *SocketOptions) GetReceiveBufferSize() int64 {
+	so.stats.GetReceiveBufferSize.Increment()
 	return so.receiveBufferSize.Load()
 }
 
@@ -718,6 +836,7 @@ func (so *SocketOptions) ReceiveBufferLimits() (min, max int64) {
 // SetReceiveBufferSize sets the value of the SO_RCVBUF option, optionally
 // notifying the owning endpoint.
 func (so *SocketOptions) SetReceiveBufferSize(receiveBufferSize int64, notify bool) {
+	so.stats.SetReceiveBufferSize.Increment()
 	var postSet func()
 	if notify {
 		oldSz := so.receiveBufferSize.Load()
@@ -731,6 +850,7 @@ func (so *SocketOptions) SetReceiveBufferSize(receiveBufferSize int64, notify bo
 
 // GetRcvlowat gets value for SO_RCVLOWAT option.
 func (so *SocketOptions) GetRcvlowat() int32 {
+	so.stats.GetRcvlowat.Increment()
 	// TODO(b/226603727): Return so.rcvlowat after adding complete support
 	// for SO_RCVLOWAT option. For now, return the default value of 1.
 	defaultRcvlowat := int32(1)
@@ -739,6 +859,7 @@ func (so *SocketOptions) GetRcvlowat() int32 {
 
 // SetRcvlowat sets value for SO_RCVLOWAT option.
 func (so *SocketOptions) SetRcvlowat(rcvlowat int32) Error {
+	so.stats.SetRcvlowat.Increment()
 	so.rcvlowat.Store(rcvlowat)
 	return nil
 }
