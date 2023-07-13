8b57c2e7402f16c784a7d14ea3f488c5f4d1ad6b
Andrei Vagin <avagin@google.com>
2023-07-13 14:39:31 -0700

    runsc/seccomp: allow sched_getaffinity if race is on
    Reported-by: syzbot+a9d200b7b966fb93070d@syzkaller.appspotmail.com
    PiperOrigin-RevId: 547926332

--- runsc/fsgofer/filter/extra_filters_race.go
+++ runsc/fsgofer/filter/extra_filters_race.go
@@ -27,17 +27,18 @@ import (
 func instrumentationFilters() seccomp.SyscallRules {
 	log.Warningf("*** SECCOMP WARNING: TSAN is enabled: syscall filters less restrictive!")
 	return archInstrumentationFilters(seccomp.SyscallRules{
-		unix.SYS_BRK:             {},
-		unix.SYS_CLOCK_NANOSLEEP: {},
-		unix.SYS_CLONE:           {},
-		unix.SYS_CLONE3:          {},
-		unix.SYS_FUTEX:           {},
-		unix.SYS_MADVISE:         {},
-		unix.SYS_MMAP:            {},
-		unix.SYS_MUNLOCK:         {},
-		unix.SYS_NANOSLEEP:       {},
-		unix.SYS_OPENAT:          {},
-		unix.SYS_RSEQ:            {},
-		unix.SYS_SET_ROBUST_LIST: {},
+		unix.SYS_BRK:               {},
+		unix.SYS_CLOCK_NANOSLEEP:   {},
+		unix.SYS_CLONE:             {},
+		unix.SYS_CLONE3:            {},
+		unix.SYS_FUTEX:             {},
+		unix.SYS_MADVISE:           {},
+		unix.SYS_MMAP:              {},
+		unix.SYS_MUNLOCK:           {},
+		unix.SYS_NANOSLEEP:         {},
+		unix.SYS_OPENAT:            {},
+		unix.SYS_RSEQ:              {},
+		unix.SYS_SET_ROBUST_LIST:   {},
+		unix.SYS_SCHED_GETAFFINITY: {},
 	})
 }
