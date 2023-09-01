17a28d3bdc240c3ff184a7ef347ffc94f8dcc51d
Lucas Manning <lucasmanning@google.com>
2023-09-01 12:00:37 -0700

    Disable broken socket_inet_loopback test.
    PiperOrigin-RevId: 562013660

--- test/syscalls/linux/socket_inet_loopback.cc
+++ test/syscalls/linux/socket_inet_loopback.cc
@@ -1868,7 +1868,8 @@ INSTANTIATE_TEST_SUITE_P(
 using SocketMultiProtocolInetLoopbackTest =
     ::testing::TestWithParam<ProtocolTestParam>;
 
-TEST_P(SocketMultiProtocolInetLoopbackTest, V4MappedLoopbackOnlyReservesV4) {
+TEST_P(SocketMultiProtocolInetLoopbackTest,
+       DISABLED_V4MappedLoopbackOnlyReservesV4) {
   ProtocolTestParam const& param = GetParam();
 
   for (int i = 0; true; i++) {
@@ -1917,7 +1918,9 @@ TEST_P(SocketMultiProtocolInetLoopbackTest, V4MappedLoopbackOnlyReservesV4) {
   }
 }
 
-TEST_P(SocketMultiProtocolInetLoopbackTest, V4MappedAnyOnlyReservesV4) {
+// TODO(b/298680322): Investigate why this fails on newer kernel versions.
+TEST_P(SocketMultiProtocolInetLoopbackTest,
+       DISABLED_V4MappedAnyOnlyReservesV4) {
   ProtocolTestParam const& param = GetParam();
 
   for (int i = 0; true; i++) {
@@ -2310,7 +2313,8 @@ TEST_P(SocketMultiProtocolInetLoopbackTest, V6EphemeralPortReserved) {
   }
 }
 
-TEST_P(SocketMultiProtocolInetLoopbackTest, V4MappedEphemeralPortReserved) {
+TEST_P(SocketMultiProtocolInetLoopbackTest,
+       DISABLED_V4MappedEphemeralPortReserved) {
   ProtocolTestParam const& param = GetParam();
 
   for (int i = 0; true; i++) {
