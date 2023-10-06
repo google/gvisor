6c24ab8dd80d7351834758b13a5a5349d301e1ba
Lucas Manning <lucasmanning@google.com>
2023-10-05 17:25:37 -0700

    Issue FUSE_FLUSH request during file close.
    PiperOrigin-RevId: 571178563

--- pkg/sentry/fsimpl/fuse/file.go
+++ pkg/sentry/fsimpl/fuse/file.go
@@ -92,6 +92,21 @@ func (fd *fileDescription) Release(ctx context.Context) {
 	conn.CallAsync(ctx, req)
 }
 
+// OnClose implements vfs.FileDescriptionImpl.OnClose.
+func (fd *fileDescription) OnClose(ctx context.Context) error {
+	inode := fd.inode()
+	conn := inode.fs.conn
+	inode.attrMu.Lock()
+	defer inode.attrMu.Unlock()
+
+	in := linux.FUSEFlushIn{
+		Fh:        fd.Fh,
+		LockOwner: 0, // TODO(gvisor.dev/issue/3245): file lock
+	}
+	req := conn.NewRequest(auth.CredentialsFromContext(ctx), pidFromContext(ctx), inode.nodeID, linux.FUSE_FLUSH, &in)
+	return conn.CallAsync(ctx, req)
+}
+
 // PRead implements vfs.FileDescriptionImpl.PRead.
 func (fd *fileDescription) PRead(ctx context.Context, dst usermem.IOSequence, offset int64, opts vfs.ReadOptions) (int64, error) {
 	return 0, nil
