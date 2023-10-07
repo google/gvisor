google/gvisor

commit eca83ac68cdac5846b3836f3e77c0a7e399491b5

Add initial support for EROFS
This patch adds initial support for EROFS [1]. Below is a brief
summary of the supported features.

Both inode formats are supported:

- compact format (32 bytes);
- extended format (64 bytes);

Below data layouts are supported:

- flat file data without data inline (no extent);
- flat file data with tail packing data inline (no extent);

Below file types are supported:

- directory;
- regular file;
- symlink;

Special files (e.g. fifo) can be listed, but cannot be accessed.

With this patch, sentry will be able to mount the EROFS image
created with below command and access the files on it.

 mkfs.erofs -E noinline_data <IMAGE FILE> <SOURCE DIRECTORY>

[1] https://docs.kernel.org/filesystems/erofs.html

Updates #8956

Signed-off-by: Tiwei Bie <tiwei.btw@antgroup.com>

pkg/sentry/fsimpl/erofs/directory.go
@@ -1,0 +1,172 @@
+// Copyright 2023 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package erofs
+
+import (
+	"sort"
+	"sync"
+
+	"gvisor.dev/gvisor/pkg/abi/linux"
+	"gvisor.dev/gvisor/pkg/context"
+	"gvisor.dev/gvisor/pkg/errors/linuxerr"
+	"gvisor.dev/gvisor/pkg/sentry/vfs"
+)
+
+func (i *inode) getDirents() ([]vfs.Dirent, error) {
+	// Fast path.
+	i.dirMu.RLock()
+	dirents := i.dirents
+	i.dirMu.RUnlock()
+	if dirents != nil {
+		return dirents, nil
+	}
+
+	// Slow path.
+	i.dirMu.Lock()
+	defer i.dirMu.Unlock()
+
+	off := int64(1)
+	if err := i.IterDirents(func(name string, typ uint8, nid uint64) error {
+		dirents = append(dirents, vfs.Dirent{
+			Name:    name,
+			Type:    linux.FileTypeToDirentType(typ),
+			Ino:     nid,
+			NextOff: off,
+		})
+		off++
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	// "." and ".." should always be present.
+	if len(dirents) < 2 {
+		return nil, linuxerr.EUCLEAN
+	}
+
+	i.dirents = dirents
+	return dirents, nil
+}
+
+func (i *inode) lookup(name string) (uint64, error) {
+	// TODO: For simplicity, currently a lookup will cause all dirents to be
+	// read and cached. But it hurts the performance of large directories.
+	// We should do binary search on disk data directly (like Linux does).
+	dirents, err := i.getDirents()
+	if err != nil {
+		return 0, err
+	}
+
+	// Skip "." and ".."
+	dirents = dirents[2:]
+
+	// The dirents are sorted in alphabetical order. We do binary search
+	// to find the target.
+	idx := sort.Search(len(dirents), func(i int) bool {
+		return dirents[i].Name >= name
+	})
+	if idx >= len(dirents) || dirents[idx].Name != name {
+		return 0, linuxerr.ENOENT
+	}
+	return dirents[idx].Ino, nil
+}
+
+func (d *dentry) lookup(ctx context.Context, name string) (*dentry, error) {
+	// Fast path, dentry already exists.
+	d.dirMu.RLock()
+	child, ok := d.childMap[name]
+	d.dirMu.RUnlock()
+	if ok {
+		return child, nil
+	}
+
+	// Slow path, create a new dentry.
+	d.dirMu.Lock()
+	defer d.dirMu.Unlock()
+	if child, ok := d.childMap[name]; ok {
+		return child, nil
+	}
+
+	nid, err := d.inode.lookup(name)
+	if err != nil {
+		return nil, err
+	}
+
+	if d.childMap == nil {
+		d.childMap = make(map[string]*dentry)
+	}
+
+	child, err = d.inode.fs.newDentry(nid)
+	if err != nil {
+		return nil, err
+	}
+	child.parent.Store(d)
+	child.name = name
+	d.childMap[name] = child
+	return child, nil
+}
+
+// +stateify savable
+type directoryFD struct {
+	fileDescription
+	vfs.DirectoryFileDescriptionDefaultImpl
+
+	// mu protects off.
+	mu sync.Mutex `state:"nosave"`
+	// +checklocks:mu
+	off int64
+}
+
+// IterDirents implements vfs.FileDescriptionImpl.IterDirents.
+func (fd *directoryFD) IterDirents(ctx context.Context, cb vfs.IterDirentsCallback) error {
+	d := fd.dentry()
+	dirents, err := d.inode.getDirents()
+	if err != nil {
+		return err
+	}
+
+	d.InotifyWithParent(ctx, linux.IN_ACCESS, 0, vfs.PathEvent)
+
+	fd.mu.Lock()
+	defer fd.mu.Unlock()
+
+	for fd.off < int64(len(dirents)) {
+		if err := cb.Handle(dirents[fd.off]); err != nil {
+			return err
+		}
+		fd.off++
+	}
+	return nil
+}
+
+// Seek implements vfs.FileDescriptionImpl.Seek.
+func (fd *directoryFD) Seek(ctx context.Context, offset int64, whence int32) (int64, error) {
+	fd.mu.Lock()
+	defer fd.mu.Unlock()
+
+	switch whence {
+	case linux.SEEK_SET:
+		// use offset as specified
+	case linux.SEEK_CUR:
+		offset += fd.off
+	default:
+		return 0, linuxerr.EINVAL
+	}
+	if offset < 0 {
+		return 0, linuxerr.EINVAL
+	}
+	fd.off = offset
+	return offset, nil
+}

pkg/sentry/fsimpl/erofs/erofs.go
@@ -1,0 +1,523 @@
+// Copyright 2023 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package erofs implements erofs.
+package erofs
+
+import (
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+	"sync/atomic"
+
+	"gvisor.dev/gvisor/pkg/abi/linux"
+	"gvisor.dev/gvisor/pkg/cleanup"
+	"gvisor.dev/gvisor/pkg/context"
+	"gvisor.dev/gvisor/pkg/erofs"
+	"gvisor.dev/gvisor/pkg/errors/linuxerr"
+	"gvisor.dev/gvisor/pkg/sentry/kernel/auth"
+	"gvisor.dev/gvisor/pkg/sentry/vfs"
+)
+
+const Name = "erofs"
+
+// Mount option names for EROFS.
+const (
+	moptImageFD = "ifd"
+)
+
+// FilesystemType implements vfs.FilesystemType.
+//
+// +stateify savable
+type FilesystemType struct{}
+
+// filesystem implements vfs.FilesystemImpl.
+//
+// +stateify savable
+type filesystem struct {
+	vfsfs vfs.Filesystem
+
+	// Immutable options.
+	mopts string
+
+	// devMinor is the filesystem's minor device number. devMinor is immutable.
+	devMinor uint32
+
+	// root is the root dentry. root is immutable.
+	root *dentry
+
+	// image is the EROFS image. image is immutable.
+	image *erofs.Image
+
+	// mf implements memmap.File for this image.
+	mf imageMemmapFile
+
+	// inodeBuckets contains the inodes in use. Multiple buckets are used to
+	// reduce the lock contention. Bucket is chosen based on the hash calculation
+	// on nid in filesystem.inodeBucket.
+	inodeBuckets []inodeBucket
+}
+
+// Name implements vfs.FilesystemType.Name.
+func (FilesystemType) Name() string {
+	return Name
+}
+
+// Release implements vfs.FilesystemType.Release.
+func (FilesystemType) Release(ctx context.Context) {}
+
+// GetFilesystem implements vfs.FilesystemType.GetFilesystem.
+func (fstype FilesystemType) GetFilesystem(ctx context.Context, vfsObj *vfs.VirtualFilesystem, creds *auth.Credentials, source string, opts vfs.GetFilesystemOptions) (*vfs.Filesystem, *vfs.Dentry, error) {
+	mopts := vfs.GenericParseMountOptions(opts.Data)
+
+	var cu cleanup.Cleanup
+	defer cu.Clean()
+
+	fd, err := getFDFromMountOptionsMap(ctx, mopts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	f := os.NewFile(uintptr(fd), "EROFS image file")
+	image, err := erofs.OpenImage(f)
+	if err != nil {
+		f.Close()
+		return nil, nil, err
+	}
+	cu.Add(func() { image.Close() })
+
+	devMinor, err := vfsObj.GetAnonBlockDevMinor()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fs := &filesystem{
+		mopts:    opts.Data,
+		image:    image,
+		devMinor: devMinor,
+		mf:       imageMemmapFile{image: image},
+	}
+	fs.vfsfs.Init(vfsObj, &fstype, fs)
+	cu.Add(func() { fs.vfsfs.DecRef(ctx) })
+
+	fs.inodeBuckets = make([]inodeBucket, runtime.GOMAXPROCS(0))
+	for i := range fs.inodeBuckets {
+		fs.inodeBuckets[i].init()
+	}
+
+	root, err := fs.newDentry(image.RootNid())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Increase the root's reference count to 2. One reference is returned to
+	// the caller, and the other is held by fs.
+	root.IncRef()
+	fs.root = root
+
+	cu.Release()
+	return &fs.vfsfs, &root.vfsd, nil
+}
+
+func getFDFromMountOptionsMap(ctx context.Context, mopts map[string]string) (int, error) {
+	ifdstr, ok := mopts[moptImageFD]
+	if !ok {
+		ctx.Warningf("erofs.getFDFromMountOptionsMap: image FD must be specified as '%s=<file descriptor>'", moptImageFD)
+		return -1, linuxerr.EINVAL
+	}
+	delete(mopts, moptImageFD)
+
+	ifd, err := strconv.Atoi(ifdstr)
+	if err != nil {
+		ctx.Warningf("erofs.getFDFromMountOptionsMap: invalid image FD: %s=%s", moptImageFD, ifdstr)
+		return -1, linuxerr.EINVAL
+	}
+
+	return ifd, nil
+}
+
+// Release implements vfs.FilesystemImpl.Release.
+func (fs *filesystem) Release(ctx context.Context) {
+	// An extra reference was held by the filesystem on the root.
+	if fs.root != nil {
+		fs.root.DecRef(ctx)
+	}
+	fs.image.Close()
+	fs.vfsfs.VirtualFilesystem().PutAnonBlockDevMinor(fs.devMinor)
+}
+
+func (fs *filesystem) statFS() linux.Statfs {
+	blockSize := int64(fs.image.BlockSize())
+	return linux.Statfs{
+		Type:         erofs.SuperBlockMagicV1,
+		NameLength:   erofs.MaxNameLen,
+		BlockSize:    blockSize,
+		FragmentSize: blockSize,
+		Blocks:       uint64(fs.image.Blocks()),
+	}
+}
+
+// +stateify savable
+type inodeBucket struct {
+	// mu protects inodeMap.
+	mu sync.RWMutex `state:"nosave"`
+
+	// inodeMap contains the inodes indexed by nid.
+	// +checklocks:mu
+	inodeMap map[uint64]*inode
+}
+
+func (ib *inodeBucket) init() {
+	ib.inodeMap = make(map[uint64]*inode) // +checklocksignore
+}
+
+// getInode returns the inode identified by nid. A reference on inode is also
+// returned to caller.
+func (ib *inodeBucket) getInode(nid uint64) *inode {
+	ib.mu.RLock()
+	defer ib.mu.RUnlock()
+	i := ib.inodeMap[nid]
+	if i != nil {
+		i.IncRef()
+	}
+	return i
+}
+
+// addInode adds the inode identified by nid into the bucket. It will first check
+// whether the old inode exists. If not, it will call newInode() to get the new inode.
+// The inode eventually saved in the bucket will be returned with a reference for caller.
+func (ib *inodeBucket) addInode(nid uint64, newInode func() *inode) *inode {
+	ib.mu.Lock()
+	defer ib.mu.Unlock()
+	if i, ok := ib.inodeMap[nid]; ok {
+		i.IncRef()
+		return i
+	}
+	i := newInode()
+	ib.inodeMap[nid] = i
+	return i
+}
+
+// removeInode removes the inode identified by nid.
+func (ib *inodeBucket) removeInode(nid uint64) {
+	ib.mu.Lock()
+	delete(ib.inodeMap, nid)
+	ib.mu.Unlock()
+}
+
+func (fs *filesystem) inodeBucket(nid uint64) *inodeBucket {
+	bucket := nid % uint64(len(fs.inodeBuckets))
+	return &fs.inodeBuckets[bucket]
+}
+
+// inode represents a filesystem object.
+//
+// Each dentry holds a reference on the inode it represents. An inode will
+// be dropped once its reference count reaches zero. We do not cache inodes
+// directly. The caching policy is implemented on top of dentries.
+//
+// +stateify savable
+type inode struct {
+	erofs.Inode
+
+	// inodeRefs is the reference count.
+	inodeRefs
+
+	// fs is the owning filesystem.
+	fs *filesystem
+
+	// dirMu protects dirents. dirents is immutable after creation.
+	dirMu sync.RWMutex `state:"nosave"`
+	// +checklocks:dirMu
+	dirents []vfs.Dirent `state:"nosave"`
+
+	// locks supports POSIX and BSD style locks.
+	locks vfs.FileLocks
+
+	// Inotify watches for this inode.
+	watches vfs.Watches
+}
+
+// getInode returns the inode identified by nid. A reference on inode is also
+// returned to caller.
+func (fs *filesystem) getInode(nid uint64) (*inode, error) {
+	bucket := fs.inodeBucket(nid)
+
+	// Fast path, inode already exists.
+	if i := bucket.getInode(nid); i != nil {
+		return i, nil
+	}
+
+	// Slow path, create a new inode.
+	//
+	// Construct the underlying inode object from the image without taking
+	// the bucket lock first to reduce the contention.
+	ino, err := fs.image.Inode(nid)
+	if err != nil {
+		return nil, err
+	}
+	return bucket.addInode(nid, func() *inode {
+		i := &inode{
+			Inode: ino,
+			fs:    fs,
+		}
+		i.InitRefs()
+		return i
+	}), nil
+
+}
+
+// DecRef should be called when you're finished with an inode.
+func (i *inode) DecRef(ctx context.Context) {
+	i.inodeRefs.DecRef(func() {
+		nid := i.Nid()
+		i.fs.inodeBucket(nid).removeInode(nid)
+	})
+}
+
+func (i *inode) checkPermissions(creds *auth.Credentials, ats vfs.AccessTypes) error {
+	return vfs.GenericCheckPermissions(creds, ats, linux.FileMode(i.Mode()), auth.KUID(i.UID()), auth.KGID(i.GID()))
+}
+
+func (i *inode) statTo(stat *linux.Statx) {
+	stat.Mask = linux.STATX_TYPE | linux.STATX_MODE | linux.STATX_NLINK |
+		linux.STATX_UID | linux.STATX_GID | linux.STATX_INO | linux.STATX_SIZE |
+		linux.STATX_BLOCKS | linux.STATX_ATIME | linux.STATX_CTIME |
+		linux.STATX_MTIME
+	stat.Blksize = i.fs.image.BlockSize()
+	stat.Nlink = i.Nlink()
+	stat.UID = i.UID()
+	stat.GID = i.GID()
+	stat.Mode = i.Mode()
+	stat.Ino = i.Nid()
+	stat.Size = i.Size()
+	stat.Blocks = (stat.Size + 511) / 512
+	stat.Mtime = linux.StatxTimestamp{
+		Sec:  int64(i.Mtime()),
+		Nsec: i.MtimeNsec(),
+	}
+	stat.Atime = stat.Mtime
+	stat.Ctime = stat.Mtime
+	stat.DevMajor = linux.UNNAMED_MAJOR
+	stat.DevMinor = i.fs.devMinor
+}
+
+func (i *inode) fileType() uint16 {
+	return i.Mode() & linux.S_IFMT
+}
+
+// dentry implements vfs.DentryImpl.
+//
+// The filesystem is read-only and currently we never drop the cached dentries
+// until the filesystem is unmounted. The reference model works like this:
+//
+//   - The initial reference count of each dentry is one, which is the reference
+//     held by the parent (so when the reference count is one, it also means that
+//     this is a cached dentry, i.e. not in use).
+//
+//   - When a dentry is used (e.g. opened by someone), its reference count will
+//     be increased and the new reference is held by caller.
+//
+//   - The reference count of root dentry is two. One reference is returned to
+//     the caller of `GetFilesystem()`, and the other is held by `fs`.
+//
+// TODO: This can lead to unbounded memory growth in sentry due to the ever-growing
+// dentry tree. We should have a dentry LRU cache, similar to what fsimpl/gofer does.
+//
+// +stateify savable
+type dentry struct {
+	vfsd vfs.Dentry
+
+	// dentryRefs is the reference count.
+	dentryRefs
+
+	// parent is this dentry's parent directory. If this dentry is
+	// a file system root, parent is nil.
+	parent atomic.Pointer[dentry] `state:".(*dentry)"`
+
+	// name is this dentry's name in its parent. If this dentry is
+	// a file system root, name is the empty string.
+	name string
+
+	// inode is the inode represented by this dentry.
+	inode *inode
+
+	// dirMu serializes changes to the dentry tree.
+	dirMu sync.RWMutex `state:"nosave"`
+
+	// childMap contains the mappings of child names to dentries if this
+	// dentry represents a directory.
+	// +checklocks:dirMu
+	childMap map[string]*dentry
+}
+
+// The caller is expected to handle dentry insertion into dentry tree.
+func (fs *filesystem) newDentry(nid uint64) (*dentry, error) {
+	i, err := fs.getInode(nid)
+	if err != nil {
+		return nil, err
+	}
+	d := &dentry{
+		inode: i,
+	}
+	d.InitRefs()
+	d.vfsd.Init(d)
+	return d, nil
+}
+
+// DecRef implements vfs.DentryImpl.DecRef.
+func (d *dentry) DecRef(ctx context.Context) {
+	d.dentryRefs.DecRef(func() {
+		d.dirMu.Lock()
+		for _, c := range d.childMap {
+			c.DecRef(ctx)
+		}
+		d.childMap = nil
+		d.dirMu.Unlock()
+		d.inode.DecRef(ctx)
+	})
+}
+
+// InotifyWithParent implements vfs.DentryImpl.InotifyWithParent.
+func (d *dentry) InotifyWithParent(ctx context.Context, events, cookie uint32, et vfs.EventType) {
+	if d.inode.IsDir() {
+		events |= linux.IN_ISDIR
+	}
+	// The ordering below is important, Linux always notifies the parent first.
+	if parent := d.parent.Load(); parent != nil {
+		parent.inode.watches.Notify(ctx, d.name, events, cookie, et, false)
+	}
+	d.inode.watches.Notify(ctx, "", events, cookie, et, false)
+}
+
+// Watches implements vfs.DentryImpl.Watches.
+func (d *dentry) Watches() *vfs.Watches {
+	return &d.inode.watches
+}
+
+// OnZeroWatches implements vfs.DentryImpl.OnZeroWatches.
+func (d *dentry) OnZeroWatches(ctx context.Context) {}
+
+func (d *dentry) open(ctx context.Context, rp *vfs.ResolvingPath, opts *vfs.OpenOptions) (*vfs.FileDescription, error) {
+	ats := vfs.AccessTypesForOpenFlags(opts)
+	if err := d.inode.checkPermissions(rp.Credentials(), ats); err != nil {
+		return nil, err
+	}
+
+	switch d.inode.fileType() {
+	case linux.S_IFREG:
+		if ats&vfs.MayWrite != 0 {
+			return nil, linuxerr.EROFS
+		}
+		var fd regularFileFD
+		fd.LockFD.Init(&d.inode.locks)
+		if err := fd.vfsfd.Init(&fd, opts.Flags, rp.Mount(), &d.vfsd, &vfs.FileDescriptionOptions{AllowDirectIO: true}); err != nil {
+			return nil, err
+		}
+		return &fd.vfsfd, nil
+
+	case linux.S_IFDIR:
+		// Can't open directories with O_CREAT.
+		if opts.Flags&linux.O_CREAT != 0 {
+			return nil, linuxerr.EISDIR
+		}
+		// Can't open directories writably.
+		if ats&vfs.MayWrite != 0 {
+			return nil, linuxerr.EISDIR
+		}
+		if opts.Flags&linux.O_DIRECT != 0 {
+			return nil, linuxerr.EINVAL
+		}
+		var fd directoryFD
+		fd.LockFD.Init(&d.inode.locks)
+		if err := fd.vfsfd.Init(&fd, opts.Flags, rp.Mount(), &d.vfsd, &vfs.FileDescriptionOptions{AllowDirectIO: true}); err != nil {
+			return nil, err
+		}
+		return &fd.vfsfd, nil
+
+	case linux.S_IFLNK:
+		// Can't open symlinks without O_PATH, which is handled at the VFS layer.
+		return nil, linuxerr.ELOOP
+
+	default:
+		return nil, linuxerr.ENXIO
+	}
+}
+
+// +stateify savable
+type fileDescription struct {
+	vfsfd vfs.FileDescription
+	vfs.FileDescriptionDefaultImpl
+	vfs.LockFD
+
+	lockLogging sync.Once `state:"nosave"`
+}
+
+func (fd *fileDescription) filesystem() *filesystem {
+	return fd.vfsfd.Mount().Filesystem().Impl().(*filesystem)
+}
+
+func (fd *fileDescription) dentry() *dentry {
+	return fd.vfsfd.Dentry().Impl().(*dentry)
+}
+
+func (fd *fileDescription) inode() *inode {
+	return fd.dentry().inode
+}
+
+// Stat implements vfs.FileDescriptionImpl.Stat.
+func (fd *fileDescription) Stat(ctx context.Context, opts vfs.StatOptions) (linux.Statx, error) {
+	var stat linux.Statx
+	fd.inode().statTo(&stat)
+	return stat, nil
+}
+
+// SetStat implements vfs.FileDescriptionImpl.SetStat.
+func (fd *fileDescription) SetStat(ctx context.Context, opts vfs.SetStatOptions) error {
+	return linuxerr.EROFS
+}
+
+// StatFS implements vfs.FileDescriptionImpl.StatFS.
+func (fd *fileDescription) StatFS(ctx context.Context) (linux.Statfs, error) {
+	return fd.filesystem().statFS(), nil
+}
+
+// ListXattr implements vfs.FileDescriptionImpl.ListXattr.
+func (fd *fileDescription) ListXattr(ctx context.Context, size uint64) ([]string, error) {
+	return nil, linuxerr.ENOTSUP
+}
+
+// GetXattr implements vfs.FileDescriptionImpl.GetXattr.
+func (fd *fileDescription) GetXattr(ctx context.Context, opts vfs.GetXattrOptions) (string, error) {
+	return "", linuxerr.ENOTSUP
+}
+
+// SetXattr implements vfs.FileDescriptionImpl.SetXattr.
+func (fd *fileDescription) SetXattr(ctx context.Context, opts vfs.SetXattrOptions) error {
+	return linuxerr.EROFS
+}
+
+// RemoveXattr implements vfs.FileDescriptionImpl.RemoveXattr.
+func (fd *fileDescription) RemoveXattr(ctx context.Context, name string) error {
+	return linuxerr.EROFS
+}
+
+// Sync implements vfs.FileDescriptionImpl.Sync.
+func (*fileDescription) Sync(context.Context) error {
+	return nil
+}
+
+// Release implements vfs.FileDescriptionImpl.Release.
+func (*fileDescription) Release(ctx context.Context) {}

pkg/abi/linux/file.go
@@ -193,6 +193,40 @@ var DirentType = abi.ValueSet{
 	DT_WHT:     "DT_WHT",
 }
 
+// Values for fs on-disk file types.
+const (
+	FT_UNKNOWN  = 0
+	FT_REG_FILE = 1
+	FT_DIR      = 2
+	FT_CHRDEV   = 3
+	FT_BLKDEV   = 4
+	FT_FIFO     = 5
+	FT_SOCK     = 6
+	FT_SYMLINK  = 7
+	FT_MAX      = 8
+)
+
+// Conversion from fs on-disk file type to dirent type.
+var direntTypeByFileType = [FT_MAX]uint8{
+	FT_UNKNOWN:  DT_UNKNOWN,
+	FT_REG_FILE: DT_REG,
+	FT_DIR:      DT_DIR,
+	FT_CHRDEV:   DT_CHR,
+	FT_BLKDEV:   DT_BLK,
+	FT_FIFO:     DT_FIFO,
+	FT_SOCK:     DT_SOCK,
+	FT_SYMLINK:  DT_LNK,
+}
+
+// FileTypeToDirentType converts the on-disk file type (FT_*) to the directory
+// entry type (DT_*).
+func FileTypeToDirentType(filetype uint8) uint8 {
+	if filetype >= FT_MAX {
+		return DT_UNKNOWN
+	}
+	return direntTypeByFileType[filetype]
+}
+
 // Values for preadv2/pwritev2.
 const (
 	// NOTE(b/120162627): gVisor does not implement the RWF_HIPRI feature, but

pkg/erofs/BUILD
@@ -1,0 +1,31 @@
+load("//tools:defs.bzl", "go_library", "go_test")
+
+package(
+    default_applicable_licenses = ["//:license"],
+    licenses = ["notice"],
+)
+
+go_library(
+    name = "erofs",
+    srcs = ["erofs.go"],
+    marshal = True,
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pkg/abi/linux",
+        "//pkg/cleanup",
+        "//pkg/errors/linuxerr",
+        "//pkg/hostarch",
+        "//pkg/log",
+        "//pkg/marshal",
+        "//pkg/marshal/primitive",
+        "//pkg/safemem",
+        "@org_golang_x_sys//unix:go_default_library",
+    ],
+)
+
+go_test(
+    name = "erofs_test",
+    size = "small",
+    srcs = ["erofs_test.go"],
+    library = ":erofs",
+)

pkg/erofs/erofs_test.go
@@ -1,0 +1,37 @@
+// Copyright 2023 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package erofs
+
+import (
+	"testing"
+)
+
+func TestOnDiskStructureSizes(t *testing.T) {
+	if sb := new(SuperBlock); sb.SizeBytes() != 128 {
+		t.Errorf("wrong super block size: want 128, got %d", sb.SizeBytes())
+	}
+
+	if i := new(InodeCompact); i.SizeBytes() != 32 {
+		t.Errorf("wrong compact inode size: want 32, got %d", i.SizeBytes())
+	}
+
+	if i := new(InodeExtended); i.SizeBytes() != 64 {
+		t.Errorf("wrong extended inode size: want 64, got %d", i.SizeBytes())
+	}
+
+	if d := new(Dirent); d.SizeBytes() != 12 {
+		t.Errorf("wrong dirent size: want 12, got %d", d.SizeBytes())
+	}
+}

pkg/sentry/fsimpl/erofs/BUILD
@@ -1,0 +1,74 @@
+load("//tools:defs.bzl", "go_library")
+load("//tools/go_generics:defs.bzl", "go_template_instance")
+
+package(
+    default_applicable_licenses = ["//:license"],
+    licenses = ["notice"],
+)
+
+go_template_instance(
+    name = "fstree",
+    out = "fstree.go",
+    package = "erofs",
+    prefix = "generic",
+    template = "//pkg/sentry/vfs/genericfstree:generic_fstree",
+    types = {
+        "Dentry": "dentry",
+    },
+)
+
+go_template_instance(
+    name = "dentry_refs",
+    out = "dentry_refs.go",
+    package = "erofs",
+    prefix = "dentry",
+    template = "//pkg/refs:refs_template",
+    types = {
+        "T": "dentry",
+    },
+)
+
+go_template_instance(
+    name = "inode_refs",
+    out = "inode_refs.go",
+    package = "erofs",
+    prefix = "inode",
+    template = "//pkg/refs:refs_template",
+    types = {
+        "T": "inode",
+    },
+)
+
+go_library(
+    name = "erofs",
+    srcs = [
+        "dentry_refs.go",
+        "directory.go",
+        "erofs.go",
+        "filesystem.go",
+        "fstree.go",
+        "inode_refs.go",
+        "regular_file.go",
+        "save_restore.go",
+    ],
+    visibility = ["//pkg/sentry:internal"],
+    deps = [
+        "//pkg/abi/linux",
+        "//pkg/atomicbitops",
+        "//pkg/cleanup",
+        "//pkg/context",
+        "//pkg/erofs",
+        "//pkg/errors/linuxerr",
+        "//pkg/fspath",
+        "//pkg/hostarch",
+        "//pkg/refs",
+        "//pkg/safemem",
+        "//pkg/sentry/fsimpl/lock",
+        "//pkg/sentry/fsutil",
+        "//pkg/sentry/kernel/auth",
+        "//pkg/sentry/memmap",
+        "//pkg/sentry/socket/unix/transport",
+        "//pkg/sentry/vfs",
+        "//pkg/usermem",
+    ],
+)

commit 15e4366487c8dd984056fb82028c85eade2048fe

erofs: fix wrong assumption on the order of on-disk directory entries
For each directory, _all_ the on-disk directory entries are _strictly_
recorded in alphabetical order. There is no special treatment of "." and
"..". Thanks to @hsiangkao for pointing out this bug.

Fixes: eca83ac68cda ("Add initial support for EROFS")
Reported-by: Gao Xiang <xiang@kernel.org>
Signed-off-by: Tiwei Bie <tiwei.btw@antgroup.com>

pkg/erofs/erofs.go
@@ -586,8 +586,7 @@ func (i *Inode) blocks() uint64 {
 }
 
 // IterDirents invokes cb on each entry in the directory represented by this inode.
-// The first two directory entries are "." and "..". The remaining directory entries
-// will be iterated in alphabetical order.
+// The directory entries will be iterated in alphabetical order.
 //
 // https://docs.kernel.org/filesystems/erofs.html#directories
 //
@@ -615,7 +614,7 @@ func (i *Inode) blocks() uint64 {
 //
 // [ (metadata block) inode | optional fields | dirent M+2 | dirent M+3 | name M+2 | name M+3 | optional padding ]
 //
-// All directory entries (except the first two: "." and "..") are _strictly_ recorded in alphabetical order.
+// All directory entries are _strictly_ recorded in alphabetical order.
 func (i *Inode) IterDirents(cb func(name string, typ uint8, nid uint64) error) error {
 	if !i.IsDir() {
 		return linuxerr.ENOTDIR

pkg/sentry/fsimpl/erofs/directory.go
@@ -69,9 +69,6 @@ func (i *inode) lookup(name string) (uint64, error) {
 		return 0, err
 	}
 
-	// Skip "." and ".."
-	dirents = dirents[2:]
-
 	// The dirents are sorted in alphabetical order. We do binary search
 	// to find the target.
 	idx := sort.Search(len(dirents), func(i int) bool {

runsc/container/container_test.go
@@ -20,6 +20,7 @@ import (
 	"io"
 	"io/ioutil"
 	"math"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path"
@@ -3157,6 +3158,15 @@ func TestMountEROFS(t *testing.T) {
 	sourceDir := filepath.Join(assetsDir, "source")
 	if err := os.Mkdir(sourceDir, 0755); err != nil {
 		t.Fatalf("os.Mkdir() failed: %v", err)
+	}
+	// Create some files with leading non-alphanumeric characters in name. It's helpful
+	// to verify the on-disk directory entries order.
+	for _, c := range []byte("!#$%&()*+,-:;<=>?@[]^_`{|}~") {
+		name := fmt.Sprintf("%s/%c_file", sourceDir, c)
+		// Create the file with random data.
+		if err := ioutil.WriteFile(name, []byte(fmt.Sprintf("%v", rand.Uint64())), 0644); err != nil {
+			t.Fatalf("error creating %q: %v", name, err)
+		}
 	}
 	testApp, err := testutil.FindFile("test/cmd/test_app/test_app")
 	if err != nil {