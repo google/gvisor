80ee2d3acca8861626a20f1ae9d44f2724500a7c
Etienne Perot <eperot@google.com>
2023-10-25 15:20:20 -0700

    Move max syscall number out of `kernel` package.
    Also add "num" at the end of it so make it clear that it's an *inclusive*
    limit. Fix the uses of it which didn't treat it as inclusive.
    
    No strong opinion on where it should go, but having it available in
    other places of the codebase is helpful. It's already in two places,
    and I'd like to have it available in two more places:
    
     - In the seccomp program compiler, so that it knows how many syscalls
       it should check for deterministic-ness and caching the results.
     - In the seccomp fuzzer, to build an initial syscall corpus.
    
    PiperOrigin-RevId: 576660986

--- pkg/sentry/seccheck/syscall.go
+++ pkg/sentry/seccheck/syscall.go
@@ -14,6 +14,10 @@
 
 package seccheck
 
+import (
+	"gvisor.dev/gvisor/pkg/abi/sentry"
+)
+
 // SyscallType is an enum that denotes different types of syscall points. There
 // are 2 types of syscall point: fully-schematized and raw. Schematizes are
 // points that have syscall specific format, e.g. open => {path, flags, mode}.
@@ -47,9 +51,7 @@ type SyscallFlagListener interface {
 }
 
 const (
-	// Copied from kernel.maxSyscallNum to avoid reverse dependency.
-	syscallsMax   = 2000
-	syscallPoints = syscallsMax * int(syscallTypesCount)
+	syscallPoints = (sentry.MaxSyscallNum + 1) * int(syscallTypesCount)
 )
 
 // Fields that are common for many syscalls.
@@ -75,7 +77,7 @@ func GetPointForSyscall(typ SyscallType, sysno uintptr) Point {
 // SyscallEnabled checks if the corresponding point for the syscall is enabled.
 func (s *State) SyscallEnabled(typ SyscallType, sysno uintptr) bool {
 	// Prevent overflow.
-	if sysno >= syscallsMax {
+	if sysno >= sentry.MaxSyscallNum {
 		return false
 	}
 	return s.Enabled(GetPointForSyscall(typ, sysno))
