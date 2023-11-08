40ee36ac4c0aaec82153ec74ca83d7fb1fbc9c5d
Kevin Krakauer <krakauer@google.com>
2023-11-08 14:01:37 -0800

    Automated rollback of changelist 580051079
    PiperOrigin-RevId: 580649803

--- runsc/sandbox/network.go
+++ runsc/sandbox/network.go
@@ -319,14 +319,13 @@ func createInterfacesAndRoutesFromNS(conn *urpc.Client, nsPath string, conf *con
 		args.FilePayload.Files = append(args.FilePayload.Files, pcap)
 	}
 
-	// Pass the host's NAT table.
-	f, err := writeNATBlob()
-	if err != nil {
-		// We warn rather than error because not every kernel has
-		// iptables enabled.
-		log.Warningf("failed to write NAT blob: %v", err)
-	} else {
+	// Pass the host's NAT table if requested.
+	if conf.ReproduceNAT {
 		args.NATBlob = true
+		f, err := writeNATBlob()
+		if err != nil {
+			return fmt.Errorf("failed to write NAT blob: %v", err)
+		}
 		args.FilePayload.Files = append(args.FilePayload.Files, f)
 	}
 
