b1432e59997dc6281ca23e8f316391906fce553e
Jamie Liu <jamieliu@google.com>
2023-12-13 16:33:59 -0800

    Minor changes to fault_test.
    - Make sigact_handler async-signal-safe.
    
    - Check that CS is correct in the ucontext_t provided to signal handlers.
    
    PiperOrigin-RevId: 590750330

--- test/syscalls/linux/fault.cc
+++ test/syscalls/linux/fault.cc
@@ -14,16 +14,20 @@
 
 #define _GNU_SOURCE 1
 #include <signal.h>
+#include <stdlib.h>
 #include <ucontext.h>
 #include <unistd.h>
 
 #include "gtest/gtest.h"
+#include "test/util/logging.h"
 #include "test/util/test_util.h"
 
 namespace gvisor {
 namespace testing {
 
 namespace {
+
+const char* g_exit_file;
 
 __attribute__((noinline)) void Fault(void) {
   volatile int* foo = nullptr;
@@ -46,24 +50,35 @@ int GetPcFromUcontext(ucontext_t* uc, uintptr_t* pc) {
 }
 
 void sigact_handler(int sig, siginfo_t* siginfo, void* context) {
+  ucontext_t* uc = reinterpret_cast<ucontext_t*>(context);
+
+#if defined(__x86_64__)
+  TEST_CHECK((uc->uc_mcontext.gregs[REG_CSGSFS] & 0xff) ==
+             0x33);  // Linux: __USER_CS
+#endif
+
   uintptr_t pc;
-  if (GetPcFromUcontext(reinterpret_cast<ucontext_t*>(context), &pc)) {
+  if (GetPcFromUcontext(uc, &pc)) {
     /* Expect Fault() to be at most 64 bytes in size. */
     uintptr_t fault_addr = reinterpret_cast<uintptr_t>(&Fault);
-    EXPECT_GE(pc, fault_addr);
-    EXPECT_LT(pc, fault_addr + 64);
-
-    // The following file is used to detect tests that exit prematurely. Since
-    // we need to call exit() here, delete the file by hand.
-    const char* exit_file = getenv("TEST_PREMATURE_EXIT_FILE");
-    if (exit_file != nullptr) {
-      ASSERT_THAT(unlink(exit_file), SyscallSucceeds());
-    }
-    exit(0);
+    TEST_CHECK(pc >= fault_addr);
+    TEST_CHECK(pc < fault_addr + 64);
   }
+
+  // The following file is used to detect tests that exit prematurely. Since
+  // we need to call _exit() here, delete the file by hand.
+  if (g_exit_file != nullptr) {
+    TEST_PCHECK(unlink(g_exit_file) == 0);
+  }
+  // exit() is not async-signal-safe, but _exit() is.
+  _exit(0);
 }
 
 TEST(FaultTest, InRange) {
+  // getenv is not async-signal-safe, so we must call it outside of the signal
+  // handler.
+  g_exit_file = getenv("TEST_PREMATURE_EXIT_FILE");
+
   // Reset the signal handler to do nothing so that it doesn't freak out
   // the test runner when we fire an alarm.
   struct sigaction sa = {};
