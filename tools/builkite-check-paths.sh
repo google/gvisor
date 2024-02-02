f5fc446f2f2f69d140b37c0cd42a0ed10c4cbb56
Andrei Vagin <avagin@google.com>
2024-02-02 13:30:14 -0800

    buildkite: don't load images if they are not touched (v2)
    v2: modify pipeline.yaml to skip load-all-images tests.
    PiperOrigin-RevId: 603773497

---
+++ tools/builkite-check-paths.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2019 The gVisor Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check if any changes are in specified paths. The script exits with 0 if no
+# changes were detected.
+
+set -xeo pipefail
+
+exec 1>&2
+
+git fetch origin master
+baseid=$(git merge-base origin/master "origin/${BUILDKITE_BRANCH}")
+
+git diff --stat "${baseid}" --exit-code "$@"
