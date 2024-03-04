eb62843a8d159ee42c00db554cb23630ea74c08d
Jing Chen <chjing@google.com>
2024-03-04 15:25:04 -0800

    Add VFIO device for VFIO container.
    PiperOrigin-RevId: 612606195

--- pkg/sentry/devices/tpuproxy/vfio.go
+++ pkg/sentry/devices/tpuproxy/vfio.go
@@ -1,10 +1,10 @@
-// Copyright 2023 The gVisor Authors.
+// Copyright 2024 The gVisor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,55 +12,69 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package tpuproxy implements proxying for TPU devices.
 package tpuproxy
 
 import (
+	"fmt"
+
+	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/context"
 	"gvisor.dev/gvisor/pkg/errors/linuxerr"
+	"gvisor.dev/gvisor/pkg/fdnotifier"
 	"gvisor.dev/gvisor/pkg/sentry/arch"
 	"gvisor.dev/gvisor/pkg/sentry/vfs"
 	"gvisor.dev/gvisor/pkg/usermem"
 	"gvisor.dev/gvisor/pkg/waiter"
 )
 
-// tpuFD implements vfs.FileDescriptionImpl for /dev/vfio/[0-9]+
-//
-// tpuFD is not savable until TPU save/restore is needed.
-type tpuFD struct {
+// deviceFD implements vfs.FileDescriptionImpl for /dev/vfio/vfio.
+type vfioFd struct {
 	vfsfd vfs.FileDescription
 	vfs.FileDescriptionDefaultImpl
 	vfs.DentryMetadataFileDescriptionImpl
 	vfs.NoLockFD
 
-	hostFD int32
-	device *tpuDevice
+	hostFd int32
+	device *vfioDevice
+	queue  waiter.Queue
 }
 
 // Release implements vfs.FileDescriptionImpl.Release.
-func (fd *tpuFD) Release(context.Context) {
+func (fd *vfioFd) Release(context.Context) {
+	fdnotifier.RemoveFD(fd.hostFd)
+	fd.queue.Notify(waiter.EventHUp)
+	unix.Close(int(fd.hostFd))
 }
 
 // EventRegister implements waiter.Waitable.EventRegister.
-func (fd *tpuFD) EventRegister(e *waiter.Entry) error {
+func (fd *vfioFd) EventRegister(e *waiter.Entry) error {
+	fd.queue.EventRegister(e)
+	if err := fdnotifier.UpdateFD(fd.hostFd); err != nil {
+		fd.queue.EventUnregister(e)
+		return err
+	}
 	return nil
 }
 
 // EventUnregister implements waiter.Waitable.EventUnregister.
-func (fd *tpuFD) EventUnregister(e *waiter.Entry) {
+func (fd *vfioFd) EventUnregister(e *waiter.Entry) {
+	fd.queue.EventUnregister(e)
+	if err := fdnotifier.UpdateFD(fd.hostFd); err != nil {
+		panic(fmt.Sprint("UpdateFD:", err))
+	}
 }
 
 // Readiness implements waiter.Waitable.Readiness.
-func (fd *tpuFD) Readiness(mask waiter.EventMask) waiter.EventMask {
-	return waiter.EventErr
+func (fd *vfioFd) Readiness(mask waiter.EventMask) waiter.EventMask {
+	return fdnotifier.NonBlockingPoll(fd.hostFd, mask)
 }
 
 // Epollable implements vfs.FileDescriptionImpl.Epollable.
-func (fd *tpuFD) Epollable() bool {
+func (fd *vfioFd) Epollable() bool {
 	return true
 }
 
 // Ioctl implements vfs.FileDescriptionImpl.Ioctl.
-func (fd *tpuFD) Ioctl(ctx context.Context, uio usermem.IO, sysno uintptr, args arch.SyscallArguments) (uintptr, error) {
+func (fd *vfioFd) Ioctl(ctx context.Context, uio usermem.IO, sysno uintptr, args arch.SyscallArguments) (uintptr, error) {
 	return 0, linuxerr.ENOSYS
 }
