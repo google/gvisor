c3391127a481e809d7bd8d63f4ed9fa08f756d75
Fabricio Voznika <fvoznika@google.com>
2024-03-12 14:12:34 -0700

    Track container name in the kernel
    This is done to allow re-mapping of container IDs after the
    container has been restored. Upon restore, container names
    remain the same, but container ID may be (and likely are)
    different.
    
    Updates #1956
    
    PiperOrigin-RevId: 615168486

--- runsc/container/state_file.go
+++ runsc/container/state_file.go
@@ -25,7 +25,6 @@ import (
 	"strings"
 
 	"github.com/gofrs/flock"
-	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/log"
 	"gvisor.dev/gvisor/pkg/sync"
 )
@@ -113,17 +112,8 @@ func Load(rootDir string, id FullID, opts LoadOpts) (*Container, error) {
 		//
 		// This is inherently racy.
 		switch c.Status {
-		case Created:
-			if !c.IsSandboxRunning() {
-				// Sandbox no longer exists, so this container definitely does not exist.
-				log.Warningf("Process for sandbox %v is no longer running; assuming container is in stopped state", c.Sandbox.ID)
-				c.changeStatus(Stopped)
-			}
-		case Running:
-			if err := c.SignalContainer(unix.Signal(0), false); err != nil {
-				log.Warningf("Cannot signal container %v for sandbox %v (err: %v); assuming container is in stopped state", c.ID, c.Sandbox.ID, err)
-				c.changeStatus(Stopped)
-			}
+		case Created, Running:
+			c.CheckStopped()
 		}
 	}
 
