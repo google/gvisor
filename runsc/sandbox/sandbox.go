5e2d2c5ed0cf343345fa5a4da8273a06b8fc3f80
Ayush Ranjan <ayushranjan@google.com>
2024-03-21 12:43:25 -0700

    Bump timeout while waiting for sandbox process to stop.
    When the sandbox process has a large memory footprint (say >25 GiB), it can
    take more than 5 seconds for the sandobx process to dissapear from the
    process table after receiving SIGKILL.
    
    This is more applicable for GPU applications which can use very large amounts
    of memory.
    
    PiperOrigin-RevId: 617931970

--- runsc/sandbox/sandbox.go
+++ runsc/sandbox/sandbox.go
@@ -1462,7 +1462,7 @@ func (s *Sandbox) waitForStopped() error {
 		return nil
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 	b := backoff.WithContext(backoff.NewConstantBackOff(100*time.Millisecond), ctx)
 	op := func() error {
