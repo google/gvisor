7fe6d155c6f550ede8a308696fad5e2670584073
Fabricio Voznika <fvoznika@google.com>
2024-04-09 11:09:35 -0700

    Automated rollback of changelist 621996848
    PiperOrigin-RevId: 623223878

--- pkg/sentry/control/proc.go
+++ pkg/sentry/control/proc.go
@@ -266,8 +266,9 @@ func (proc *Proc) execAsync(args *ExecArgs) (*kernel.ThreadGroup, kernel.ThreadI
 		initArgs.Filename = resolved
 	}
 
-	containerName := proc.Kernel.ContainerNameGivenID(args.ContainerID)
-	ttyFile, err := fdimport.Import(ctx, fdTable, args.StdioIsPty, args.KUID, args.KGID, fdMap, containerName)
+	// TODO(gvisor.dev/issue/1956): Container name is not really needed because
+	// exec processes are not restored, but add it for completeness.
+	ttyFile, err := fdimport.Import(ctx, fdTable, args.StdioIsPty, args.KUID, args.KGID, fdMap, "")
 	if err != nil {
 		return nil, 0, nil, err
 	}
