google/gvisor

commit 64268c8483651ea83fc8783b9f1ec1bac0366b0a

netstack: make tcpip.Address an opaque type
The important change here is in tcpip/tcpip.go, where tcpip.Address is defined.
The rest is updating uses of tcpip.Address.

This is preparation for netip.Addr or []byte based addresses, which should save
us a bunch of allocations. Currently, we allocate every time we want to, say,
get a tcpip.Address from a header. This is because the header is a byte slice,
but Address is a string. Strings are immutable, so Go allocates and copies.

PiperOrigin-RevId: 532284732

pkg/sentry/socket/socket.go
@@ -49,8 +49,8 @@ type ControlMessages struct {
 func packetInfoToLinux(packetInfo tcpip.IPPacketInfo) linux.ControlMessageIPPacketInfo {
 	var p linux.ControlMessageIPPacketInfo
 	p.NIC = int32(packetInfo.NIC)
-	copy(p.LocalAddr[:], packetInfo.LocalAddr)
-	copy(p.DestinationAddr[:], packetInfo.DestinationAddr)
+	copy(p.LocalAddr[:], packetInfo.LocalAddr.AsSlice())
+	copy(p.DestinationAddr[:], packetInfo.DestinationAddr.AsSlice())
 	return p
 }
 
@@ -58,7 +58,7 @@ func packetInfoToLinux(packetInfo tcpip.IPPacketInfo) linux.ControlMessageIPPack
 // format.
 func ipv6PacketInfoToLinux(packetInfo tcpip.IPv6PacketInfo) linux.ControlMessageIPv6PacketInfo {
 	var p linux.ControlMessageIPv6PacketInfo
-	if n := copy(p.Addr[:], packetInfo.Addr); n != len(p.Addr) {
+	if n := copy(p.Addr[:], packetInfo.Addr.AsSlice()); n != len(p.Addr) {
 		panic(fmt.Sprintf("got copy(%x, %x) = %d, want = %d", p.Addr, packetInfo.Addr, n, len(p.Addr)))
 	}
 	p.NIC = uint32(packetInfo.NIC)
@@ -99,14 +99,14 @@ func sockErrCmsgToLinux(sockErr *tcpip.SockError) linux.SockErrCMsg {
 	switch sockErr.NetProto {
 	case header.IPv4ProtocolNumber:
 		errMsg := &linux.SockErrCMsgIPv4{SockExtendedErr: ee}
-		if len(sockErr.Offender.Addr) > 0 {
+		if len(sockErr.Offender.Addr.AsSlice()) > 0 {
 			addr, _ := ConvertAddress(linux.AF_INET, sockErr.Offender)
 			errMsg.Offender = *addr.(*linux.SockAddrInet)
 		}
 		return errMsg
 	case header.IPv6ProtocolNumber:
 		errMsg := &linux.SockErrCMsgIPv6{SockExtendedErr: ee}
-		if len(sockErr.Offender.Addr) > 0 {
+		if len(sockErr.Offender.Addr.AsSlice()) > 0 {
 			addr, _ := ConvertAddress(linux.AF_INET6, sockErr.Offender)
 			errMsg.Offender = *addr.(*linux.SockAddrInet6)
 		}
@@ -461,7 +461,8 @@ func Htons(v uint16) uint16 {
 // case when it has the fe80::/10 prefix. This check is used to determine when
 // the NICID is relevant for a given IPv6 address.
 func isLinkLocal(addr tcpip.Address) bool {
-	return len(addr) >= 2 && addr[0] == 0xfe && addr[1]&0xc0 == 0x80
+	addrBytes := addr.AsSlice()
+	return len(addrBytes) >= 2 && addrBytes[0] == 0xfe && addrBytes[1]&0xc0 == 0x80
 }
 
 // ConvertAddress converts the given address to a native format.
@@ -469,20 +470,21 @@ func ConvertAddress(family int, addr tcpip.FullAddress) (linux.SockAddr, uint32)
 	switch family {
 	case linux.AF_INET:
 		var out linux.SockAddrInet
-		copy(out.Addr[:], addr.Addr)
+		copy(out.Addr[:], addr.Addr.AsSlice())
 		out.Family = linux.AF_INET
 		out.Port = Htons(addr.Port)
 		return &out, uint32(sockAddrInetSize)
 
 	case linux.AF_INET6:
 		var out linux.SockAddrInet6
-		if len(addr.Addr) == header.IPv4AddressSize {
+		addrBytes := addr.Addr.AsSlice()
+		if len(addrBytes) == header.IPv4AddressSize {
 			// Copy address in v4-mapped format.
-			copy(out.Addr[12:], addr.Addr)
+			copy(out.Addr[12:], addrBytes)
 			out.Addr[10] = 0xff
 			out.Addr[11] = 0xff
 		} else {
-			copy(out.Addr[:], addr.Addr)
+			copy(out.Addr[:], addrBytes)
 		}
 		out.Family = linux.AF_INET6
 		out.Port = Htons(addr.Port)
@@ -496,7 +498,7 @@ func ConvertAddress(family int, addr tcpip.FullAddress) (linux.SockAddr, uint32)
 		out.Family = linux.AF_PACKET
 		out.InterfaceIndex = int32(addr.NIC)
 		out.HardwareAddrLen = header.EthernetAddressSize
-		copy(out.HardwareAddr[:], addr.Addr)
+		copy(out.HardwareAddr[:], addr.Addr.AsSlice())
 		return &out, uint32(sockAddrLinkSize)
 
 	default:
@@ -508,9 +510,9 @@ func ConvertAddress(family int, addr tcpip.FullAddress) (linux.SockAddr, uint32)
 // netstack representation taking any addresses into account.
 func BytesToIPAddress(addr []byte) tcpip.Address {
 	if bytes.Equal(addr, make([]byte, 4)) || bytes.Equal(addr, make([]byte, 16)) {
-		return ""
+		return tcpip.Address{}
 	}
-	return tcpip.Address(addr)
+	return tcpip.AddrFromSlice(addr)
 }
 
 // AddressAndFamily reads an sockaddr struct from the given address and
@@ -526,22 +528,6 @@ func AddressAndFamily(addr []byte) (tcpip.FullAddress, uint16, *syserr.Error) {
 
 	// Get the rest of the fields based on the address family.
 	switch family := hostarch.ByteOrder.Uint16(addr); family {
-	case linux.AF_UNIX:
-		path := addr[2:]
-		if len(path) > linux.UnixPathMax {
-			return tcpip.FullAddress{}, family, syserr.ErrInvalidArgument
-		}
-		// Drop the terminating NUL (if one exists) and everything after
-		// it for filesystem (non-abstract) addresses.
-		if len(path) > 0 && path[0] != 0 {
-			if n := bytes.IndexByte(path[1:], 0); n >= 0 {
-				path = path[:n+1]
-			}
-		}
-		return tcpip.FullAddress{
-			Addr: tcpip.Address(path),
-		}, family, nil
-
 	case linux.AF_INET:
 		var a linux.SockAddrInet
 		if len(addr) < sockAddrInetSize {
@@ -584,8 +570,15 @@ func AddressAndFamily(addr []byte) (tcpip.FullAddress, uint16, *syserr.Error) {
 		}
 
 		return tcpip.FullAddress{
-			NIC:  tcpip.NICID(a.InterfaceIndex),
-			Addr: tcpip.Address(a.HardwareAddr[:header.EthernetAddressSize]),
+			NIC: tcpip.NICID(a.InterfaceIndex),
+			// This is a hack. FullAddress is designed to carry IP
+			// addresses, but it's overloaded here to carry a link
+			// address. We stick the 6 byte link address to 10
+			// zeroed bytes.
+			Addr: tcpip.AddrFrom16Slice(append(
+				a.HardwareAddr[:header.EthernetAddressSize],
+				[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...,
+			)),
 			Port: Ntohs(a.Protocol),
 		}, family, nil
 

pkg/sentry/socket/unix/unix.go
@@ -17,6 +17,7 @@
 package unix
 
 import (
+	"bytes"
 	"fmt"
 
 	"golang.org/x/sys/unix"
@@ -435,7 +436,7 @@ func (s *Socket) Endpoint() transport.Endpoint {
 
 // extractPath extracts and validates the address.
 func extractPath(sockaddr []byte) (string, *syserr.Error) {
-	addr, family, err := socket.AddressAndFamily(sockaddr)
+	addr, family, err := addressAndFamily(sockaddr)
 	if err != nil {
 		if err == syserr.ErrAddressFamilyNotSupported {
 			err = syserr.ErrInvalidArgument
@@ -447,7 +448,7 @@ func extractPath(sockaddr []byte) (string, *syserr.Error) {
 	}
 
 	// The address is trimmed by GetAddress.
-	p := string(addr.Addr)
+	p := addr.Addr
 	if p == "" {
 		// Not allowed.
 		return "", syserr.ErrInvalidArgument
@@ -458,6 +459,33 @@ func extractPath(sockaddr []byte) (string, *syserr.Error) {
 	}
 
 	return p, nil
+}
+
+func addressAndFamily(addr []byte) (transport.Address, uint16, *syserr.Error) {
+	// Make sure we have at least 2 bytes for the address family.
+	if len(addr) < 2 {
+		return transport.Address{}, 0, syserr.ErrInvalidArgument
+	}
+
+	// Get the rest of the fields based on the address family.
+	switch family := hostarch.ByteOrder.Uint16(addr); family {
+	case linux.AF_UNIX:
+		path := addr[2:]
+		if len(path) > linux.UnixPathMax {
+			return transport.Address{}, family, syserr.ErrInvalidArgument
+		}
+		// Drop the terminating NUL (if one exists) and everything after
+		// it for filesystem (non-abstract) addresses.
+		if len(path) > 0 && path[0] != 0 {
+			if n := bytes.IndexByte(path[1:], 0); n >= 0 {
+				path = path[:n+1]
+			}
+		}
+		return transport.Address{
+			Addr: string(path),
+		}, family, nil
+	}
+	return transport.Address{}, 0, syserr.ErrAddressFamilyNotSupported
 }
 
 // GetPeerName implements the linux syscall getpeername(2) for sockets backed by

pkg/sentry/strace/socket.go
@@ -352,7 +352,7 @@ func sockAddr(t *kernel.Task, addr hostarch.Addr, length uint32) string {
 		}
 
 		if family == linux.AF_UNIX {
-			return fmt.Sprintf("%#x {Family: %s, Addr: %q}", addr, familyStr, string(fa.Addr))
+			return fmt.Sprintf("%#x {Family: %s, Addr: %q}", addr, familyStr, string(fa.Addr.AsSlice()))
 		}
 
 		return fmt.Sprintf("%#x {Family: %s, Addr: %v, Port: %d}", addr, familyStr, fa.Addr, fa.Port)

commit 876de08e592a934f4f77561c2f55e34916f61950

Fix strace logging for AF_UNIX sockets.
This was broken in commit 64268c8, which moved parsing of addresses for
AF_UNIX sockets to the socket/unix package.

While we're here, add strace support for AF_PACKET sockets, since
socket.AddressAndFamily can already decode their address.

PiperOrigin-RevId: 661391690

pkg/sentry/socket/socket.go
@@ -516,8 +516,8 @@ func BytesToIPAddress(addr []byte) tcpip.Address {
 }
 
 // AddressAndFamily reads an sockaddr struct from the given address and
-// converts it to the FullAddress format. It supports AF_UNIX, AF_INET,
-// AF_INET6, and AF_PACKET addresses.
+// converts it to the FullAddress format. It supports AF_INET, AF_INET6, and
+// AF_PACKET addresses.
 //
 // AddressAndFamily returns an address and its family.
 func AddressAndFamily(addr []byte) (tcpip.FullAddress, uint16, *syserr.Error) {

pkg/sentry/socket/unix/unix.go
@@ -445,7 +445,7 @@ func (s *Socket) Endpoint() transport.Endpoint {
 
 // extractPath extracts and validates the address.
 func extractPath(sockaddr []byte) (string, *syserr.Error) {
-	addr, family, err := addressAndFamily(sockaddr)
+	addr, family, err := AddressAndFamily(sockaddr)
 	if err != nil {
 		if err == syserr.ErrAddressFamilyNotSupported {
 			err = syserr.ErrInvalidArgument
@@ -466,7 +466,9 @@ func extractPath(sockaddr []byte) (string, *syserr.Error) {
 	return p, nil
 }
 
-func addressAndFamily(addr []byte) (transport.Address, uint16, *syserr.Error) {
+// AddressAndFamily converts the addr byte slice to a transport.Address and
+// family. It supports only AF_UNIX addresses.
+func AddressAndFamily(addr []byte) (transport.Address, uint16, *syserr.Error) {
 	// Make sure we have at least 2 bytes for the address family.
 	if len(addr) < 2 {
 		return transport.Address{}, 0, syserr.ErrInvalidArgument

pkg/sentry/strace/BUILD
@@ -39,6 +39,7 @@ go_library(
         "//pkg/sentry/kernel",
         "//pkg/sentry/socket",
         "//pkg/sentry/socket/netlink",
+        "//pkg/sentry/socket/unix",
         "//pkg/sentry/syscalls/linux",
     ],
 )

pkg/sentry/strace/socket.go
@@ -26,6 +26,7 @@ import (
 	"gvisor.dev/gvisor/pkg/sentry/kernel"
 	"gvisor.dev/gvisor/pkg/sentry/socket"
 	"gvisor.dev/gvisor/pkg/sentry/socket/netlink"
+	"gvisor.dev/gvisor/pkg/sentry/socket/unix"
 	slinux "gvisor.dev/gvisor/pkg/sentry/syscalls/linux"
 )
 
@@ -345,17 +346,18 @@ func sockAddr(t *kernel.Task, addr hostarch.Addr, length uint32) string {
 	familyStr := SocketFamily.Parse(uint64(family))
 
 	switch family {
-	case linux.AF_INET, linux.AF_INET6, linux.AF_UNIX:
+	case linux.AF_INET, linux.AF_INET6, linux.AF_PACKET:
 		fa, _, err := socket.AddressAndFamily(b)
 		if err != nil {
 			return fmt.Sprintf("%#x {Family: %s, error extracting address: %v}", addr, familyStr, err)
 		}
-
-		if family == linux.AF_UNIX {
-			return fmt.Sprintf("%#x {Family: %s, Addr: %q}", addr, familyStr, string(fa.Addr.AsSlice()))
-		}
-
 		return fmt.Sprintf("%#x {Family: %s, Addr: %v, Port: %d}", addr, familyStr, fa.Addr, fa.Port)
+	case linux.AF_UNIX:
+		fa, _, err := unix.AddressAndFamily(b)
+		if err != nil {
+			return fmt.Sprintf("%#x {Family: %s, error extracting address: %v}", addr, familyStr, err)
+		}
+		return fmt.Sprintf("%#x {Family: %s, Addr: %q}", addr, familyStr, fa.Addr)
 	case linux.AF_NETLINK:
 		sa, err := netlink.ExtractSockAddr(b)
 		if err != nil {