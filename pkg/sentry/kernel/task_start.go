693258e5b99215729b1a891b5656e137f59b4d8e
Etienne Perot <etienne@perot.me>
2024-08-20 10:38:47 -0700

    Implement `personality(2)` for no-op personality bits.
    This PR implements the [`personality(2)` syscall](https://www.man7.org/linux/man-pages/man2/personality.2.html) for personality bits that are no-ops on Linux.
    
    Fixes #10756.
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/gvisor/pull/10757 from EtiennePerot:linux-is-not-a-personality-trait 91d366501bc672ca13a0076018bcdfbab06f5a8e
    PiperOrigin-RevId: 665427818

--- pkg/sentry/kernel/task_start.go
+++ pkg/sentry/kernel/task_start.go
@@ -104,6 +104,9 @@ type TaskConfig struct {
 	// It may be nil.
 	SessionKeyring *auth.Key
 
+	// Personality is the personality of the parent task.
+	Personality uint32
+
 	Origin TaskOrigin
 }
 
@@ -174,6 +177,7 @@ func (ts *TaskSet) newTask(ctx context.Context, cfg *TaskConfig) (*Task, error) 
 		cgroups:        make(map[Cgroup]struct{}),
 		userCounters:   cfg.UserCounters,
 		sessionKeyring: cfg.SessionKeyring,
+		personality:    atomicbitops.FromUint32(cfg.Personality),
 		Origin:         cfg.Origin,
 	}
 	t.netns = cfg.NetworkNamespace
