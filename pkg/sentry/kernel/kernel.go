eca66af461d345332184edcb149ad258469872e7
Ayush Ranjan <ayushranjan@google.com>
2024-10-10 11:31:07 -0700

    Rename OCIEnviron to SpecEnviron.
    PiperOrigin-RevId: 684516121

--- pkg/sentry/kernel/kernel.go
+++ pkg/sentry/kernel/kernel.go
@@ -396,7 +396,7 @@ type Kernel struct {
 // Saver is an interface for saving the kernel.
 type Saver interface {
 	SaveAsync() error
-	OCIEnviron(containerName string) []string
+	SpecEnviron(containerName string) []string
 }
 
 // InitKernelArgs holds arguments to Init.
