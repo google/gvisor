7fb3b706c0e4a649350768f0315d41f2b5755fec
Etienne Perot <eperot@google.com>
2025-04-03 15:55:28 -0700

    Read pages from the main MemoryFile as early as possible during restore
    This change modifies the sandbox restore process such that if the memory
    pages are stored as separate files, their contents are optimistically read
    from these files as soon as possible during the sandbox restore process,
    as opposed to the previous behavior where they were read only after the last
    container of the sandbox had been restored.
    
    When memory page data is stored as separate files, the process of restoring
    multiple `MemoryFile`s is a bit more intertwined than it appears, for two
    reasons:
    
    - The pages metadata file stores data for all `MemoryFile`s one after the
      other. So we must also plumb the pages metadata file reader that is used
      for reading the main `MemoryFile` metadata to the code that also loads the
      private `MemoryFile`s, and ensure that these operations are correctly
      serialized, since the primary `MemoryFile` metadata comes earlier in the
      file.
    - The `MemoryFile` page contents are all in the same file, and the byte offset
      within this file at which each each `MemoryFile` begins must be known before
      kicking off async loading of that MemoryFile.
    
    So this change introduces a `PagesFileLoader`, which is meant to be a
    thread-safe struct that keeps track of this process. It owns the FDs for the
    pages and page metadata files and handles the serialization of the operations
    executed using these FDs.
    
    Support for the previous checkpoint/restore format (with all data in a single
    file) is maintained using a synchronous alternative that implements the same
    interface but simply loads everything from the shared state file reader when
    the time to do so comes around.
    
    PiperOrigin-RevId: 743727664

--- pkg/sentry/kernel/kernel_restore.go
+++ pkg/sentry/kernel/kernel_restore.go
@@ -15,7 +15,16 @@
 package kernel
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+
+	"gvisor.dev/gvisor/pkg/context"
+	"gvisor.dev/gvisor/pkg/fd"
 	"gvisor.dev/gvisor/pkg/log"
+	"gvisor.dev/gvisor/pkg/sentry/pgalloc"
+	"gvisor.dev/gvisor/pkg/state"
 	"gvisor.dev/gvisor/pkg/sync"
 )
 
@@ -191,4 +200,237 @@ func (w *CheckpointWaitable) signal(gen CheckpointGeneration, err error) {
 			waiter.callback = nil
 		}
 	}
+}
+
+// PagesFileLoader coordinates the process of loading pages into MemoryFiles,
+// possibly asynchronously.
+//
+// It is used to load pages from checkpoint (whether through a separate file
+// or the main state file) into one or more MemoryFiles.
+//
+// Lifecycle:
+// PagesFileLoader may be created as soon as a primary MemoryFile is known
+// and a state file is opened.
+//
+// The PagesFileLoader has three jobs:
+//   - Loading the main MemoryFile as early as possible.
+//   - Loading the private MemoryFiles, also as early as possible but after
+//     the main MemoryFile.
+//   - Ensuring that the above is done by a certain point of the restore
+//     process.
+type PagesFileLoader interface {
+	// KickoffPrivate attempts to kick off loading of private MemoryFiles
+	// asynchronously, if possible. If not, this function is a no-op.
+	// The private MemoryFile data must be known and within `ctx` by the point
+	// this function is called.
+	// This function does not need to be called within the lifecycle of
+	// PagesFileLoader, but if it is, it must be called before Load.
+	KickoffPrivate(ctx context.Context) error
+
+	// Load loads the main MemoryFile and all private MemoryFiles.
+	//
+	// If loading operations were not kicked off asynchronously, this function
+	// will perform them synchronously. `background` is ignored in this case.
+	//
+	// If they were kicked off asynchronously, this function will wait for them
+	// instead, with `background` controlling how early this function returns:
+	//
+	//   - If `background` is false, this function will return after both pages
+	//     metadata and pages themselves have been loaded.
+	//   - If `background` is true, this function will return after the main
+	//     MemoryFile and all private MemoryFiles have had their metadata
+	//     loaded, but will not wait for their pages to be loaded.
+	//
+	// Load must be called exactly once, and no further functions may be called
+	// on PagesFileLoader after Load is called.
+	Load(ctx context.Context, mainMf *pgalloc.MemoryFile, background bool) error
+}
+
+// singleStateFilePFL implements PagesFileLoader by loading MemoryFiles from a
+// single state file.
+type singleStateFilePFL struct {
+	// source is the checkpoint source.
+	source io.Reader
+}
+
+// KickoffPrivate is a no-op for this implementation.
+func (*singleStateFilePFL) KickoffPrivate(ctx context.Context) error {
+	return nil
+}
+
+// loadPrivateMemoryFiles loads the private MemoryFiles from ctx, which must
+// contain MemoryFileMap metadata, and it reads private MemoryFile metadata
+// from `r`. This consumes bytes from `r`, so this must be called only once.
+func loadPrivateMemoryFiles(ctx context.Context, r io.Reader, mfmap map[string]*pgalloc.MemoryFile, opts *pgalloc.LoadOpts) error {
+	// Load the metadata.
+	var meta privateMemoryFileMetadata
+	if _, err := state.Load(ctx, r, &meta); err != nil {
+		return err
+	}
+	// Ensure that it is consistent with CtxFilesystemMemoryFileMap.
+	if len(mfmap) != len(meta.owners) {
+		return fmt.Errorf("inconsistent private memory files on restore: savedMFOwners = %v, CtxFilesystemMemoryFileMap = %v", meta.owners, mfmap)
+	}
+	// Load all private memory files.
+	for _, fsID := range meta.owners {
+		mf, ok := mfmap[fsID]
+		if !ok {
+			return fmt.Errorf("saved private memory file for %q was not configured on restore", fsID)
+		}
+		err := mf.LoadFrom(ctx, r, opts)
+		if err != nil {
+			return fmt.Errorf("failed to load MemoryFile %p fsID %q: %w", mf, fsID, err)
+		}
+	}
+	return nil
+}
+
+// Load loads the main MemoryFile and all private MemoryFiles synchronously.
+func (pfl *singleStateFilePFL) Load(ctx context.Context, mainMf *pgalloc.MemoryFile, background bool) error {
+	var opts pgalloc.LoadOpts
+	if err := mainMf.LoadFrom(ctx, pfl.source, &opts); err != nil {
+		return fmt.Errorf("failed to load main MemoryFile %p: %w", mainMf, err)
+	}
+	if err := loadPrivateMemoryFiles(ctx, pfl.source, pgalloc.MemoryFileMapFromContext(ctx), &opts); err != nil {
+		return fmt.Errorf("failed to load private MemoryFiles: %w", err)
+	}
+	return nil
+}
+
+// NewSingleStateFilePagesFileLoader creates a new PagesFileLoader that loads
+// MemoryFiles from a single state file synchronously.
+// It does not take ownership of `source`, so the caller is responsible for
+// closing it.
+func NewSingleStateFilePagesFileLoader(source io.Reader) PagesFileLoader {
+	return &singleStateFilePFL{
+		source: source,
+	}
+}
+
+// separatePagesFileLoader implements PagesFileLoader by loading MemoryFiles
+// asynchronously thanks to having separate pages and page metadata files,
+// as opposed to a single state file containing everything.
+type separatePagesFileLoader struct {
+	// loadPrivateMFs is used to communicate with the background goroutine
+	// once private MemoryFiles are known.
+	loadPrivateMFs sync.Once
+
+	// privateMFsChan is used to tell the background goroutine about
+	// private MemoryFiles, once they are known.
+	// This channel is written to exactly once as part of `loadPrivateMFs`.
+	privateMFsChan chan map[string]*pgalloc.MemoryFile
+
+	// loadResultCh is the channel used by the background goroutine to report
+	// load errors. In normal successful operation, this channel will be written
+	// exactly two `nil` values: one once all MemoryFile *metadata* has been
+	// loaded, and one when all MemoryFile pages have been loaded.
+	// Then the channel will be closed.
+	// If any actual error occurs, this channel will have this error written to
+	// it, and then closed.
+	loadResultCh chan error
+}
+
+// NewSeparatePagesFileLoader creates a new PageFileLoader that loads
+// MemoryFiles asynchronously from separate pages and page metadata files.
+// It takes ownership of pagesMetadata and pagesFile.
+func NewSeparatePagesFileLoader(pagesMetadata, pagesFile *fd.FD, mainMF *pgalloc.MemoryFile) PagesFileLoader {
+	pfl := &separatePagesFileLoader{
+		privateMFsChan: make(chan map[string]*pgalloc.MemoryFile, 1),
+		loadResultCh:   make(chan error, 2),
+	}
+	go func() {
+		defer close(pfl.loadResultCh)
+		defer pagesMetadata.Close()
+		defer pagesFile.Close()
+		pfl.loadResultCh <- pfl.backgroundGoroutine(pagesMetadata, pagesFile, mainMF)
+	}()
+	return pfl
+}
+
+func (pfl *separatePagesFileLoader) backgroundGoroutine(pagesMetadataFD, pagesFileFD *fd.FD, mainMF *pgalloc.MemoryFile) error {
+	ctx := context.Background()
+	var wg sync.WaitGroup
+	var loadErrsMu sync.Mutex
+	var loadErrs []error
+
+	// //pkg/state/wire reads one byte at a time; buffer these reads to
+	// avoid making one syscall per read. For the "main" state file, this
+	// buffering is handled by statefile.NewReader() => compressio.Reader
+	// or compressio.NewSimpleReader().
+	pagesMetadata := bufio.NewReader(pagesMetadataFD)
+
+	opts := pgalloc.LoadOpts{
+		PagesFile: pagesFileFD,
+		OnAsyncPageLoadStart: func(mf *pgalloc.MemoryFile) {
+			wg.Add(1)
+			log.Infof("Starting async page load for %p", mf)
+		},
+		OnAsyncPageLoadDone: func(mf *pgalloc.MemoryFile, err error) {
+			defer wg.Done()
+			if err != nil {
+				log.Warningf("Async page load error for %p: %v", mf, err)
+				loadErrsMu.Lock()
+				loadErrs = append(loadErrs, fmt.Errorf("%p: async page load: %w", mf, err))
+				loadErrsMu.Unlock()
+			}
+		},
+	}
+
+	log.Infof("Loading metadata for main MemoryFile: %p", mainMF)
+	if err := mainMF.LoadFrom(ctx, pagesMetadata, &opts); err != nil {
+		log.Warningf("Failed to load main MemoryFile %p: %v", mainMF, err)
+		return err
+	}
+
+	privateMFs := <-pfl.privateMFsChan
+	log.Infof("Loading metadata for %d private MemoryFiles", len(privateMFs))
+	if err := loadPrivateMemoryFiles(ctx, pagesMetadata, privateMFs, &opts); err != nil {
+		log.Warningf("Failed to load private MemoryFiles: %v", err)
+		return err
+	}
+
+	// Report metadata load completion.
+	log.Infof("All MemoryFile metadata has been loaded")
+	pfl.loadResultCh <- nil
+
+	// Wait for page loads to complete and report errors.
+	wg.Wait()
+	loadErrsMu.Lock()
+	defer loadErrsMu.Unlock()
+	if loadErr := errors.Join(loadErrs...); loadErr != nil {
+		return loadErr
+	}
+	log.Infof("All MemoryFile pages have been loaded.")
+	return nil
+}
+
+// KickoffPrivate implements PagesFileLoader.KickoffPrivate.
+func (pfl *separatePagesFileLoader) KickoffPrivate(ctx context.Context) error {
+	pfl.loadPrivateMFs.Do(func() {
+		pfl.privateMFsChan <- pgalloc.MemoryFileMapFromContext(ctx)
+	})
+	return nil
+}
+
+// Load implements PagesFileLoader.Load.
+func (pfl *separatePagesFileLoader) Load(ctx context.Context, mainMf *pgalloc.MemoryFile, background bool) error {
+	if err := pfl.KickoffPrivate(ctx); err != nil {
+		return err
+	}
+
+	// First error from loadResultCh is about MemoryFile metadata.
+	if err := <-pfl.loadResultCh; err != nil {
+		return fmt.Errorf("failed to load MemoryFile metadata: %w", err)
+	}
+
+	// Second error from loadResultCh is about MemoryFile pages.
+	// We only wait for it if `background=false`.
+	if !background {
+		if err := <-pfl.loadResultCh; err != nil {
+			return fmt.Errorf("failed to load MemoryFile pages: %w", err)
+		}
+		return nil
+	}
+	log.Infof("All page load work has been kicked off successfully in the background.")
+	return nil
 }
