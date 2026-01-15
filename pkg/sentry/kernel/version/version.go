f01a2fea134a2a0772cdfc7aafed646779b53c16
Champ-Goblem <cameron@northflank.com>
2026-01-15 11:20:03 +0000

    Sentry: Add support for sysfs osrelease, ostype, version The linux kernel outputs the current release version to `/proc/sys/kernel/osrelease`. Some libraries read this file to determine which features to enable. Implement the `osrelease`, `ostype`, `version` files to match what is returned for uname.
    Fixes: #12487

---
+++ pkg/sentry/kernel/version/version.go
@@ -0,0 +1,27 @@
+// Copyright 2026 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package version holds the kernel versioning information.
+package version
+
+const (
+	// LinuxSysname is the OS name advertised by gVisor.
+	LinuxSysname = "Linux"
+
+	// LinuxRelease is the Linux release version number advertised by gVisor.
+	LinuxRelease = "4.4.0"
+
+	// LinuxVersion is the version info advertised by gVisor.
+	LinuxVersion = "#1 SMP Sun Jan 10 15:06:54 PST 2016"
+)
