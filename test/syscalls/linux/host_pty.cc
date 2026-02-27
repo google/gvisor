4fae23b8acd12e745982b38c378ec6e368b2e561
Jeremy Maitin-Shepard <jbms@google.com>
2026-02-27 15:47:48 -0800

    devpts: Support TCGETS2, TCSETS2, TCSETSW2, TCSETSF2
    glibc 2.42 uses TCGETS2 in isatty, and fails to properly detect ttys
    without TCGETS2 supported.
    
    This commit also adds support for treating additional host TTY file descriptors (not just stdin/stdout/stderr) passed to a container process as TTYs inside the sandbox.  Previously, such file descriptors were not considered TTYs inside the sandbox.
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/gvisor/pull/12589 from jbms:tcgets2 b8a4f245618ec01f876f7c22d0bb33d02d5563eb
    PiperOrigin-RevId: 876446717

---
+++ test/syscalls/linux/host_pty.cc
@@ -0,0 +1,69 @@
+// Copyright 2026 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/strings/numbers.h"
+#include "test/util/pty_util.h"
+#include "test/util/test_util.h"
+
+namespace gvisor {
+namespace testing {
+
+namespace {
+
+TEST(HostPtyTest, Termios2) {
+  // We expect a host PTY FD to be passed.
+  char* fd_str = getenv("TEST_HOST_PTY_FD");
+  ASSERT_NE(fd_str, nullptr) << "TEST_HOST_PTY_FD environment variable not set";
+  int fd;
+  ASSERT_TRUE(absl::SimpleAtoi(fd_str, &fd))
+      << "Invalid TEST_HOST_PTY_FD: " << fd_str;
+
+  struct termios t;
+  ASSERT_THAT(ioctl(fd, TCGETS, &t), SyscallSucceeds());
+
+  struct kernel_termios2 t2 = {};
+  ASSERT_THAT(ioctl(fd, TCGETS2, &t2), SyscallSucceeds());
+
+  EXPECT_EQ(t.c_iflag, t2.c_iflag);
+  EXPECT_EQ(t.c_oflag, t2.c_oflag);
+  EXPECT_EQ(t.c_cflag, t2.c_cflag);
+  EXPECT_EQ(t.c_lflag, t2.c_lflag);
+  for (int i = 0; i < NCCS && i < KERNEL_NCCS; ++i) {
+    EXPECT_EQ(t.c_cc[i], t2.c_cc[i]);
+  }
+
+  // Test TCSETS2.
+  auto original_lflag = t2.c_lflag;
+  t2.c_lflag ^= ECHO;
+  ASSERT_THAT(ioctl(fd, TCSETS2, &t2), SyscallSucceeds());
+
+  struct kernel_termios2 t3 = {};
+  ASSERT_THAT(ioctl(fd, TCGETS2, &t3), SyscallSucceeds());
+  EXPECT_EQ(t2.c_lflag, t3.c_lflag);
+
+  // Restore original flags.
+  t2.c_lflag = original_lflag;
+  ASSERT_THAT(ioctl(fd, TCSETS2, &t2), SyscallSucceeds());
+}
+
+}  // namespace
+
+}  // namespace testing
+}  // namespace gvisor
