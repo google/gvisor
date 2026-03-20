5d1a93651c21ceb8bc835523c91f3adeb58f6639
atoniolo76 <alessio@modal.com>
2026-03-20 16:27:37 +0000

    Cst hostFD to int for compatibility

--- pkg/sentry/devices/rdmaproxy/rdmaproxy.go
+++ pkg/sentry/devices/rdmaproxy/rdmaproxy.go
@@ -53,7 +53,7 @@ func (dev *uverbsDevice) Open(ctx context.Context, mnt *vfs.Mount, vfsd *vfs.Den
 		unix.Close(hostFD)
 		return nil, err
 	}
-	fd.memmapFile.SetFD(fd.hostFD)
+	fd.memmapFile.SetFD(int(fd.hostFD))
 	if err := fd.vfsfd.Init(fd, opts.Flags, auth.CredentialsFromContext(ctx), mnt, vfsd, &vfs.FileDescriptionOptions{
 		UseDentryMetadata: true,
 	}); err != nil {
