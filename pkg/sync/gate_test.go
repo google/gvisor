google/gvisor

commit ed8bdf461b5f8093957a11f526032d42938af7ca

Consolidate most synchronization primitive linknames in the sync package.
PiperOrigin-RevId: 345359823

pkg/sync/race_arm64.s
@@ -12,21 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build race
+// +build arm64
+
 #include "textflag.h"
 
-// See waiter_noasm_unsafe.go for a description of waiterUnlock.
-//
-// func waiterUnlock(ptr unsafe.Pointer, wg *unsafe.Pointer) bool
-TEXT ·waiterUnlock(SB),NOSPLIT,$0-24
-	MOVQ ptr+0(FP), DI
-	MOVQ wg+8(FP), SI
-
-	MOVQ $·preparingG(SB), AX
-	LOCK
-	CMPXCHGQ DI, 0(SI)
-
-	SETEQ AX
-	MOVB AX, ret+16(FP)
-
+// func RaceUncheckedAtomicCompareAndSwapUintptr(ptr *uintptr, old, new uintptr) bool
+TEXT ·RaceUncheckedAtomicCompareAndSwapUintptr(SB),NOSPLIT,$0-25
+	MOVD ptr+0(FP), R0
+	MOVD old+8(FP), R1
+	MOVD new+16(FP), R1
+again:
+	LDAXR (R0), R3
+	CMP R1, R3
+	BNE ok
+	STLXR R2, (R0), R3
+	CBNZ R3, again
+ok:
+	CSET EQ, R0
+	MOVB R0, ret+24(FP)
 	RET
 

pkg/sleep/BUILD
@@ -5,10 +5,6 @@ package(licenses = ["notice"])
 go_library(
     name = "sleep",
     srcs = [
-        "commit_amd64.s",
-        "commit_arm64.s",
-        "commit_asm.go",
-        "commit_noasm.go",
         "sleep_unsafe.go",
     ],
     visibility = ["//:sandbox"],

pkg/sleep/commit_amd64.s
@@ -1,35 +1,0 @@
-// Copyright 2018 The gVisor Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include "textflag.h"
-
-#define preparingG 1
-
-// See commit_noasm.go for a description of commitSleep.
-//
-// func commitSleep(g uintptr, waitingG *uintptr) bool
-TEXT ·commitSleep(SB),NOSPLIT,$0-24
-	MOVQ waitingG+8(FP), CX
-	MOVQ g+0(FP), DX
-
-	// Store the G in waitingG if it's still preparingG. If it's anything
-	// else it means a waker has aborted the sleep.
-	MOVQ $preparingG, AX
-	LOCK
-	CMPXCHGQ DX, 0(CX)
-
-	SETEQ AX
-	MOVB AX, ret+16(FP)
-
-	RET

pkg/sleep/commit_arm64.s
@@ -1,38 +1,0 @@
-// Copyright 2019 The gVisor Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include "textflag.h"
-
-#define preparingG 1
-
-// See commit_noasm.go for a description of commitSleep.
-//
-// func commitSleep(g uintptr, waitingG *uintptr) bool
-TEXT ·commitSleep(SB),NOSPLIT,$0-24
-	MOVD waitingG+8(FP), R0
-	MOVD $preparingG, R1
-	MOVD G+0(FP), R2
-
-	// Store the G in waitingG if it's still preparingG. If it's anything
-	// else it means a waker has aborted the sleep.
-again:
-        LDAXR   (R0), R3
-        CMP     R1, R3
-        BNE     ok
-        STLXR   R2, (R0), R3
-        CBNZ    R3, again
-ok:
-        CSET    EQ, R0
-        MOVB    R0, ret+16(FP)
-        RET

pkg/sleep/commit_asm.go
@@ -1,20 +1,0 @@
-// Copyright 2018 The gVisor Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build amd64 arm64
-
-package sleep
-
-// See commit_noasm.go for a description of commitSleep.
-func commitSleep(g uintptr, waitingG *uintptr) bool

pkg/sleep/commit_noasm.go
@@ -1,33 +1,0 @@
-// Copyright 2018 The gVisor Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build !race
-// +build !amd64,!arm64
-
-package sleep
-
-import "sync/atomic"
-
-// commitSleep signals to wakers that the given g is now sleeping. Wakers can
-// then fetch it and wake it.
-//
-// The commit may fail if wakers have been asserted after our last check, in
-// which case they will have set s.waitingG to zero.
-//
-// It is written in assembly because it is called from g0, so it doesn't have
-// a race context.
-func commitSleep(g uintptr, waitingG *uintptr) bool {
-	// Try to store the G so that wakers know who to wake.
-	return atomic.CompareAndSwapUintptr(waitingG, preparingG, g)
-}

pkg/sleep/empty.s
@@ -1,15 +1,0 @@
-// Copyright 2018 The gVisor Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Empty assembly file so empty func definitions work.

pkg/sleep/sleep_unsafe.go
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build go1.11
-// +build !go1.17
-
-// Check go:linkname function signatures when updating Go version.
-
 // Package sleep allows goroutines to efficiently sleep on multiple sources of
 // notifications (wakers). It offers O(1) complexity, which is different from
 // multi-channel selects which have O(n) complexity (where n is the number of
@@ -90,12 +85,6 @@ var (
 	// wakers that are asserted.
 	assertedSleeper Sleeper
 )
-
-//go:linkname gopark runtime.gopark
-func gopark(unlockf func(uintptr, *uintptr) bool, wg *uintptr, reason uint8, traceEv byte, traceskip int)
-
-//go:linkname goready runtime.goready
-func goready(g uintptr, traceskip int)
 
 // Sleeper allows a goroutine to sleep and receive wake up notifications from
 // Wakers in an efficient way.
@@ -189,7 +178,7 @@ func (s *Sleeper) nextWaker(block bool) *Waker {
 			// See:runtime2.go in the go runtime package for
 			// the values to pass as the waitReason here.
 			const waitReasonSelect = 9
-			gopark(commitSleep, &s.waitingG, waitReasonSelect, traceEvGoBlockSelect, 0)
+			sync.Gopark(commitSleep, unsafe.Pointer(&s.waitingG), sync.WaitReasonSelect, sync.TraceEvGoBlockSelect, 0)
 		}
 
 		// Pull the shared list out and reverse it in the local
@@ -210,6 +199,18 @@ func (s *Sleeper) nextWaker(block bool) *Waker {
 	s.localList = w.next
 
 	return w
+}
+
+// commitSleep signals to wakers that the given g is now sleeping. Wakers can
+// then fetch it and wake it.
+//
+// The commit may fail if wakers have been asserted after our last check, in
+// which case they will have set s.waitingG to zero.
+//
+//go:norace
+//go:nosplit
+func commitSleep(g uintptr, waitingG unsafe.Pointer) bool {
+	return sync.RaceUncheckedAtomicCompareAndSwapUintptr((*uintptr)(waitingG), preparingG, g)
 }
 
 // Fetch fetches the next wake-up notification. If a notification is immediately
@@ -311,7 +312,7 @@ func (s *Sleeper) enqueueAssertedWaker(w *Waker) {
 	case 0, preparingG:
 	default:
 		// We managed to get a G. Wake it up.
-		goready(g, 0)
+		sync.Goready(g, 0)
 	}
 }
 

commit 1b2946c4fa59c1a64643bce962140b5686062601

checklocks: fix arm64 race CAS operands
The race-only ARM64 CAS introduced by ed8bdf461b5 overwrites the
expected value in R1 while its conditional store reads R2. Load the
replacement into R2 so the comparison and store use distinct operands.

Gate, Sleeper and Waiter use this primitive in their park callbacks.
Failed comparisons can prevent parking and let Gate.Close return before
its users have left. Exercise successful replacement and rejection
without mutation through the same API in the existing Gate tests.

Assisted-by: Codex

pkg/sync/race_arm64.s
@@ -21,7 +21,7 @@
 TEXT ·RaceUncheckedAtomicCompareAndSwapUintptr(SB),NOSPLIT,$0-25
 	MOVD ptr+0(FP), R0
 	MOVD old+8(FP), R1
-	MOVD new+16(FP), R1
+	MOVD new+16(FP), R2
 again:
 	LDAXR (R0), R3
 	CMP R1, R3

pkg/sync/gate_test.go
@@ -37,6 +37,18 @@ func TestGateBasic(t *testing.T) {
 	}
 }
 
+func TestRaceUncheckedAtomicCompareAndSwapUintptr(t *testing.T) {
+	// Gate's park callback needs distinct old and new operands even when the
+	// primitive uses race-detector-free assembly.
+	value := uintptr(1)
+	if swapped := RaceUncheckedAtomicCompareAndSwapUintptr(&value, 1, 2); !swapped || value != 2 {
+		t.Fatalf("CAS(1, 2) = %t, value = %d; want true, 2", swapped, value)
+	}
+	if swapped := RaceUncheckedAtomicCompareAndSwapUintptr(&value, 1, 3); swapped || value != 2 {
+		t.Fatalf("CAS(1, 3) = %t, value = %d; want false, 2", swapped, value)
+	}
+}
+
 func TestGateConcurrent(t *testing.T) {
 	// Each call to testGateConcurrentOnce tests behavior around a single call
 	// to Gate.Close, so run many short tests to increase the probability of