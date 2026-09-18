500cc3a2102ccf44add68a442cefbb1293b6a88d
Kai Wang <kwang@apache.org>
2026-09-19 00:38:21 +0800

    netstack: simplify GRO flush with removeOldest

--- pkg/tcpip/stack/gro/gro.go
+++ pkg/tcpip/stack/gro/gro.go
@@ -548,13 +548,11 @@ func (gd *GRO) bucketForPacket6(ipHdr header.IPv6, tcpHdr header.TCP) int {
 // Flush sends all packets up the stack.
 func (gd *GRO) Flush() {
 	for i := range gd.buckets {
-		for groPkt := gd.buckets[i].packets.Front(); groPkt != nil; {
-			next := groPkt.Next()
-			pkt := groPkt.pkt
-			gd.buckets[i].removeOne(groPkt)
+		b := &gd.buckets[i]
+		for b.count > 0 {
+			pkt := b.removeOldest()
 			gd.handlePacket(pkt)
 			pkt.DecRef()
-			groPkt = next
 		}
 	}
 }
