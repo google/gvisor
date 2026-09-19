26f3455a4cb9a377354c33aeac049080a077c79c
Etienne Perot <eperot@google.com>
2026-09-18 22:39:35 -0700

    Enforce lint step in Buildkite pipeline.
    PiperOrigin-RevId: 984259825

--- test/syscalls/linux/socket_unix_seqpacket.cc
+++ test/syscalls/linux/socket_unix_seqpacket.cc
@@ -30,6 +30,7 @@
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "test/syscalls/linux/unix_domain_socket_test_util.h"
+#include "test/util/posix_error.h"
 #include "test/util/socket_util.h"
 #include "test/util/test_util.h"
 
